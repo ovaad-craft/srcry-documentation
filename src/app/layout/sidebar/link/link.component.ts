e5d993267c156828bd901731cc6f1ce0e155e1a2
Optiq01 <Optiqvision@gmail.com>
2023-07-24 17:09:23 -0500

    create dropdown animation

--- src/app/layout/sidebar/link/link.component.ts
+++ src/app/layout/sidebar/link/link.component.ts
@@ -1,13 +1,23 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {  trigger, state, style, animate, transition } from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { Link } from '@site-types';
+
+export const DropdownAnimation = trigger('dropdownAnimation',[
+    state('open', style({height: '*'})),
+    state('closed', style({height: '0px'})),
+    transition('open <=> closed', [animate('300ms ease-in-out')])
+  ]
+);
 
 @Component({
   selector: 'nav-link',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BrowserAnimationsModule],
   templateUrl: './link.component.html',
-  styleUrls: ['./link.component.css']
+  styleUrls: ['./link.component.css'],
+  animations: [DropdownAnimation]
 })
 export class LinkComponent {
   @Input() Link!: Link;
