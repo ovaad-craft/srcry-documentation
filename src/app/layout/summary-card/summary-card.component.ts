0ee8cfde2c361a1efbfdbc7f270611ae2e785627
Optiq01 <Optiqvision@gmail.com>
2023-08-01 12:34:17 -0500

    create summary-card

---
+++ src/app/layout/summary-card/summary-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-summary-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './summary-card.component.html',
+  styleUrls: ['./summary-card.component.css']
+})
+export class SummaryCardComponent {
+
+}
