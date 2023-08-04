fa49cd3e805890b2afcfe6a45dd3fc66914218f6
Optiq01 <Optiqvision@gmail.com>
2023-08-03 22:12:14 -0500

    create pagination data object and add to introduction page

--- src/app/pages/site/introduction-page/introduction-page.component.ts
+++ src/app/pages/site/introduction-page/introduction-page.component.ts
@@ -3,8 +3,23 @@ import { CommonModule } from '@angular/common';
 import { IntroductionPageService } from './introduction-page.service';
 import { SidebarService } from 'src/app/layout/sidebar/sidebar.service';
 import { SummaryCardComponent } from 'src/app/layout/summary-card/summary-card.component';
-import { CardSummaryData } from '@site-types';
+import { CardSummaryData, PaginationData } from '@site-types';
 import { PaginateComponent } from 'src/app/layout/paginate/paginate.component';
+
+
+const PAGINATIONDATA: PaginationData = {
+  previous:{
+    id: 'boxSize',
+    title: 'Box Size Library',
+    path: 'box-size-library',
+    breadcrumbs: ['libraries']
+  },
+  next: {
+    id: 'gettingStarted',
+    title: 'Getting Started',
+    path: 'getting-started'
+  }
+}
 
 @Component({
   selector: 'introduction-page',
@@ -18,10 +33,12 @@ export class IntroductionPageComponent implements AfterViewInit {
   LibraryDescriptionData!: CardSummaryData[];
   ClassDescriptionData!: CardSummaryData[];
   @ViewChild('hero', {read: ElementRef, static: true}) Hero!: ElementRef;
+  Pagination!: PaginationData;
 
   constructor(private homeService: IntroductionPageService, private navService: SidebarService){
     this.homeService.LibraryDescriptionData.subscribe(a=> this.LibraryDescriptionData = a);
     this.homeService.ClassDescriptionData.subscribe(a=> this.ClassDescriptionData = a);
+    this.Pagination = PAGINATIONDATA;
   }
 
   ngAfterViewInit(): void {
