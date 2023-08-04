10167ccc6d0b9e57d03ae4d6e16196fbf9ddfd05
Optiq01 <Optiqvision@gmail.com>
2023-08-04 17:28:40 -0500

    create responsive-window component

---
+++ src/app/layout/responsive-window/responsive-window.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-responsive-window',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './responsive-window.component.html',
+  styleUrls: ['./responsive-window.component.css']
+})
+export class ResponsiveWindowComponent {
+
+}
