eb5e32ec112a94ccee8e101702301c5549a05993
Optiq01 <Optiqvision@gmail.com>
2023-08-03 20:20:42 -0500

    create paginate component

---
+++ src/app/layout/paginate/paginate.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-paginate',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './paginate.component.html',
+  styleUrls: ['./paginate.component.css']
+})
+export class PaginateComponent {
+
+}
