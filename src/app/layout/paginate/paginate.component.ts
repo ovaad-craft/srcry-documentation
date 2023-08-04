9a4307d728572dd7e5fd92d77666727d78bc1ca8
Optiq01 <Optiqvision@gmail.com>
2023-08-03 21:50:29 -0500

    create navigation functionality

--- src/app/layout/paginate/paginate.component.ts
+++ src/app/layout/paginate/paginate.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PaginationData } from '@site-types';
+import { SidebarService } from '../sidebar/sidebar.service';
 
 @Component({
   selector: 'paginate',
@@ -9,5 +11,25 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./paginate.component.css']
 })
 export class PaginateComponent {
+
+  @Input() PageData!: PaginationData;
+
+  constructor(private navService: SidebarService){}
+
+  public previousPage(): void{
+    this.navService.updatePath(this.PageData.previous!.path);
+    this.navService.updateRoute(this.PageData.previous!.path);
+    if(this.PageData.previous!.breadcrumbs){
+      this.navService.updateBreadCrumbs(this.PageData.previous!.breadcrumbs);
+    }
+  }
+
+  public nextPage(): void{
+    this.navService.updatePath(this.PageData.next!.path);
+    this.navService.updateRoute(this.PageData.next!.path);
+    if(this.PageData.next!.breadcrumbs){
+      this.navService.updateBreadCrumbs(this.PageData.next!.breadcrumbs);
+    }
+  }
 
 }
