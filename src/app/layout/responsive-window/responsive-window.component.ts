46787ade358726e036ee785ee111576f24f204fe
Optiq01 <Optiqvision@gmail.com>
2023-08-04 22:21:51 -0500

    add core APIs and Inputs

--- src/app/layout/responsive-window/responsive-window.component.ts
+++ src/app/layout/responsive-window/responsive-window.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,7 +6,8 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './responsive-window.component.html',
-  styleUrls: ['./responsive-window.component.css']
+  styleUrls: ['./responsive-window.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ResponsiveWindowComponent {
 
