3cc0dd9458a47ae9cf5006155474c166810f04fb
Optiq01 <Optiqvision@gmail.com>
2023-08-13 17:25:46 -0500

    create nav-link component for demonstration

---
+++ src/app/responsive-examples/navigation/nav-link/nav-link.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-nav-link',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav-link.component.html',
+  styleUrls: ['./nav-link.component.css']
+})
+export class NavLinkComponent {
+
+}
