31818433ac1c4eaeaf71171b9a0f4ef2d99c1b3e
Optiq01 <Optiqvision@gmail.com>
2023-08-16 03:00:37 -0500

    style product component

--- src/app/responsive-examples/product/product.component.ts
+++ src/app/responsive-examples/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,7 +6,8 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ProductComponent {
 
