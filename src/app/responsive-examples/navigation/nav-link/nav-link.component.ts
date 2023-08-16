974cf66153bc9b21b487ad596736b7e65c9dbac7
Optiq01 <Optiqvision@gmail.com>
2023-08-15 20:44:11 -0500

    create nav link component for navigation component

--- src/app/responsive-examples/navigation/nav-link/nav-link.component.ts
+++ src/app/responsive-examples/navigation/nav-link/nav-link.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-nav-link',
+  selector: 'nav-link',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './nav-link.component.html',
-  styleUrls: ['./nav-link.component.css']
+  styleUrls: ['./nav-link.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class NavLinkComponent {
+
+  @Input() Link! : string;
 
 }
