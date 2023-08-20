c1534151fd43918471095da155a28119479e7767
Optiq01 <Optiqvision@gmail.com>
2023-08-20 16:02:55 -0500

    add custom options for highlight.js

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,23 @@ import { AppComponent } from './app/app.component';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { SITEROUTES } from './app/routes';
+import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(SITEROUTES),
-    provideAnimations()
+    provideAnimations(),
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        coreLibraryLoader: () => import('highlight.js/lib/core'),
+        lineNumbersLoader: () => import('ngx-highlightjs/line-numbers'),
+        languages: {
+          css: () => import('highlight.js/lib/languages/css')
+        }
+      }
+    }
   ]
 };
 
