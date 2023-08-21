1bee21407c5b833f84bf3d2818c8cf4633be5ce3
Optiq01 <Optiqvision@gmail.com>
2023-08-21 00:45:19 -0500

    add markup and styling to getting started page

--- src/app/pages/site/getting-started-page/getting-started-page.component.ts
+++ src/app/pages/site/getting-started-page/getting-started-page.component.ts
@@ -1,14 +1,52 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import{ HighlightModule, HighlightLoader } from 'ngx-highlightjs';
+import { PaginateComponent } from 'src/app/layout/paginate/paginate.component';
+import { PaginationData } from '@site-types';
+
+const PAGINATIONDATA: PaginationData = {
+  previous: {
+    id: 'introduction',
+    title: 'Introduction',
+    path: '/home'
+  },
+  next: {
+    id: 'boxSize',
+    title: 'Box Size Library',
+    path: '/box-size-library',
+    breadcrumbs: ['libraries']
+  }
+}
 
 @Component({
   selector: 'app-getting-started-page',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [
+    CommonModule,
+    RouterLink,
+    HighlightModule,
+    PaginateComponent
+  ],
   templateUrl: './getting-started-page.component.html',
   styleUrls: ['./getting-started-page.component.css']
 })
 export class GettingStartedPageComponent {
+
+  code: string =
+  `@import url('path/to/folder/srcry.css');
+
+.demoClass{
+  display: grid;
+  place-self: center;
+}
+`;
+
+Pagination!: PaginationData;
+
+  constructor(private hljsLoader: HighlightLoader){
+    this.hljsLoader.setTheme('node_modules/highlight.js/styles/a11y-dark.css');
+    this.Pagination = PAGINATIONDATA;
+  }
 
 }
