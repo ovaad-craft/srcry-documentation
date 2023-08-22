173660153d36a57e0b00782d1216c21f11c09f44
Optiq01 <Optiqvision@gmail.com>
2023-08-22 00:16:10 -0500

    markup and style syntax-frame component

--- src/app/layout/syntax-frame/syntax-frame.component.ts
+++ src/app/layout/syntax-frame/syntax-frame.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,4 +10,5 @@ import { CommonModule } from '@angular/common';
 })
 export class SyntaxFrameComponent {
 
+  @Input() Parts!: string[];
 }
