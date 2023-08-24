c81f9c9c8fcb67344e0ffdd75a38118391eacfc0
Optiq01 <Optiqvision@gmail.com>
2023-08-24 12:45:00 -0500

    create control input for box-size-analyzer

---
+++ src/app/responsive-examples/box-size-analyzer/analyzer-input/analyzer-input.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-analyzer-input',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './analyzer-input.component.html',
+  styleUrls: ['./analyzer-input.component.css']
+})
+export class AnalyzerInputComponent {
+
+}
