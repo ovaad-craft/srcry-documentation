ae235b948e1cd315777b58dbde89a97d480973fb
Optiq01 <Optiqvision@gmail.com>
2023-08-24 20:22:57 -0500

    create properties and inputs for broadcast channel

--- src/app/responsive-examples/box-size-analyzer/analyzer-input/analyzer-input.component.ts
+++ src/app/responsive-examples/box-size-analyzer/analyzer-input/analyzer-input.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-analyzer-input',
+  selector: 'analyzer-input',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './analyzer-input.component.html',
   styleUrls: ['./analyzer-input.component.css']
 })
 export class AnalyzerInputComponent {
+
+  @Input() BroadcastName!: string;
+  @Input() ChannelName!: string;
 
 }
--- src/app/responsive-examples/box-size-analyzer/analyzer-input/analyzer-input.service.ts
+++ src/app/responsive-examples/box-size-analyzer/analyzer-input/analyzer-input.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnalyzerInputService {
 
+  DataChannel!: BroadcastChannel;
+  ComponentChannel: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
   constructor() { }
+
+  public createDataChannel(dataChannelName: string, componentChannelName: string): void{
+    this.DataChannel = new BroadcastChannel(dataChannelName);
+    this.ComponentChannel.next(componentChannelName);
+  }
+
+  public closeDataChannel():void{ this.DataChannel.close(); }
 }
