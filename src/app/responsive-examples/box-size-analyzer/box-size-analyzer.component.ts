b09a169c2cda5ebed7af2d90c53ae0557032cf55
Optiq01 <Optiqvision@gmail.com>
2023-08-24 20:25:02 -0500

    create functionality for updating values

--- src/app/responsive-examples/box-size-analyzer/box-size-analyzer.component.ts
+++ src/app/responsive-examples/box-size-analyzer/box-size-analyzer.component.ts
@@ -1,10 +1,10 @@
-import { Component, ViewEncapsulation, ViewChild, ElementRef, ChangeDetectionStrategy, ChangeDetectorRef, AfterViewInit, AfterViewChecked, AfterContentChecked, OnInit, NgZone } from '@angular/core';
+import { Component, Input, ViewEncapsulation, ViewChild, ElementRef, ChangeDetectionStrategy, ChangeDetectorRef, AfterViewInit, AfterViewChecked, AfterContentChecked, OnInit, NgZone, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoxSizeAnalyzerService } from './box-size-analyzer.service';
-import { BoxAnalyzerInterface } from '@site-types';
+import { BoxAnalyzerInterface, BoxAnalyzerPropSizes } from '@site-types';
 
 @Component({
-  selector: 'app-box-size-analyzer',
+  selector: 'box-size-analyzer',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './box-size-analyzer.component.html',
@@ -12,8 +12,12 @@ import { BoxAnalyzerInterface } from '@site-types';
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.ShadowDom
 })
-export class BoxSizeAnalyzerComponent implements OnInit, AfterViewInit, AfterViewChecked, AfterContentChecked {
+export class BoxSizeAnalyzerComponent implements OnInit, AfterViewInit, AfterViewChecked, AfterContentChecked, OnDestroy {
 
+  @Input() BroadcastName!: string;
+  @Input() ChannelName!: string;
+
+  PropValues!: BoxAnalyzerPropSizes;
   BoxSize: BoxAnalyzerInterface = {coreBoxWidth: 0, fullBoxWidth: 0};
   InitSize: boolean = false;
 
@@ -25,12 +29,15 @@ export class BoxSizeAnalyzerComponent implements OnInit, AfterViewInit, AfterVie
     private boxService: BoxSizeAnalyzerService,
     private cdr: ChangeDetectorRef,
     private zone: NgZone
-  ){}
+  ){
+    this.boxService.BoxSizeValue$.subscribe(a=> this.PropValues = a);
+  }
 
   ngOnInit(): void {
   }
   
   ngAfterViewInit(): void {
+    console.log(this.BroadcastName, this.ChannelName);
     const frameObserver = new ResizeObserver(()=>{
       this.zone.run(()=> this.getBoxSizes());
     });
@@ -52,6 +59,10 @@ export class BoxSizeAnalyzerComponent implements OnInit, AfterViewInit, AfterVie
       }
   }
 
+  ngOnDestroy(): void {
+      this.boxService.closeDataChannel();
+  }
+
   private getBoxSizes(): void{
     const size: BoxAnalyzerInterface = {
       coreBoxWidth: this.CoreBox.nativeElement.offsetWidth,
@@ -59,7 +70,7 @@ export class BoxSizeAnalyzerComponent implements OnInit, AfterViewInit, AfterVie
     };
 
     this.BoxSize = size;
-    console.log(this.BoxSize);
+    //console.log(this.BoxSize);
 
   }
  
--- src/app/responsive-examples/box-size-analyzer/box-size-analyzer.service.ts
+++ src/app/responsive-examples/box-size-analyzer/box-size-analyzer.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { BoxAnalyzerPropSizes, BoxSizeInterface, BoxSizeProps } from '@site-types';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { createBoxSize } from 'src/app/utils/create-box-size';
+import { createCssVariable } from 'src/app/utils/create-css-variable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BoxSizeAnalyzerService {
 
+  DataChannel!: BroadcastChannel;
+  ComponentChannel: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
+  private BoxSizeValue: BehaviorSubject<BoxAnalyzerPropSizes> = new BehaviorSubject<BoxAnalyzerPropSizes>({
+    core: 'var(--xTiny-1-1)',
+    full: 'var(--xTiny-1-5)'
+  });
+  BoxSizeValue$ = this.BoxSizeValue.asObservable();
+
   constructor() { }
+
+  public createDataChannel(dataChannelName: string, componentChannelName: string): void{
+    this.DataChannel = new BroadcastChannel(dataChannelName);
+    this.ComponentChannel.next(componentChannelName);
+  }
+
+  public closeDataChannel():void{ this.DataChannel.close(); }
+
+  private setSizes(newSize: BoxSizeInterface): void{
+    this.BoxSizeValue.next({
+      core: createCssVariable(createBoxSize(newSize.size, '1', '1')),
+      full: createCssVariable(createBoxSize(newSize.size, newSize.scale, newSize.speed))
+    });
+  }
+
+
 }
