53c375a24243ce5226cd0c23784af6fe4ad85ef6
Optiq01 <Optiqvision@gmail.com>
2023-08-25 22:50:20 -0500

    create box size data

--- src/app/responsive-examples/box-size-analyzer/analyzer-input/box-size-selector/box-size-selector.service.ts
+++ src/app/responsive-examples/box-size-analyzer/analyzer-input/box-size-selector/box-size-selector.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { BoxSizeScale, BoxSizeSize, BoxSizeSpeed } from '@site-types';
+
+export const BOXSIZESIZES: BoxSizeSize[] = [
+  'micro', 'xTiny', 'tiny', 'xSmall', 'small', 'loMed', 'hiMed', 'large', 'jumbo'
+]
+export const BOXSIZESCALE: BoxSizeScale[] = ['1', '2']
+export const BOXSIZESPEED: BoxSizeSpeed[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']
 
 @Injectable({
   providedIn: 'root'
 })
-export class BoxSizeSelectorService {
+export class BoxSizeSelectorService implements OnInit {
+
+  BoxSizeSizes! : BoxSizeSize[];
+  BoxSizeScale! : BoxSizeScale[];
+  BoxSizeSpeed! : BoxSizeSpeed[];
 
   constructor() { }
+
+  ngOnInit(): void {
+      this.BoxSizeSizes = BOXSIZESIZES;
+      this.BoxSizeScale = BOXSIZESCALE;
+      this.BoxSizeSpeed = BOXSIZESPEED;
+  }
 }
