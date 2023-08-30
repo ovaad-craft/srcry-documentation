51e9493397f0463a524213bf6d11eca04e239d72
Optiq01 <Optiqvision@gmail.com>
2023-08-29 23:40:03 -0500

    create text-size-analyzer

---
+++ src/app/responsive-examples/text-size-analyzer/text-size-analyzer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-text-size-analyzer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './text-size-analyzer.component.html',
+  styleUrls: ['./text-size-analyzer.component.css']
+})
+export class TextSizeAnalyzerComponent {
+
+}
