c1fb5a4c85d09fa7f0e7c1d7f550063afb9b7350
Optiq01 <Optiqvision@gmail.com>
2023-09-03 15:08:04 -0500

    create line size analyzer

---
+++ src/app/responsive-examples/line-size-analyzer/line-size-analyzer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-line-size-analyzer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './line-size-analyzer.component.html',
+  styleUrls: ['./line-size-analyzer.component.css']
+})
+export class LineSizeAnalyzerComponent {
+
+}
