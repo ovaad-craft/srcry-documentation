a025630ea8ad202c8bbea67c452aeb8fcb094dd6
Optiq01 <Optiqvision@gmail.com>
2023-09-03 14:04:27 -0500

    markup and style line size demo a

--- src/app/responsive-examples/line-size-demo-a/line-size-demo-a.component.ts
+++ src/app/responsive-examples/line-size-demo-a/line-size-demo-a.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-line-size-demo-a',
+  selector: 'line-size-demo-a',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './line-size-demo-a.component.html',
-  styleUrls: ['./line-size-demo-a.component.css']
+  styleUrls: ['./line-size-demo-a.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class LineSizeDemoAComponent {
 
