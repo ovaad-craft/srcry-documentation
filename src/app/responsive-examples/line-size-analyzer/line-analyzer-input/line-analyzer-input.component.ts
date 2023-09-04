766fb5b6b1f0c3c6dd764c516d85d24871009e7f
Optiq01 <Optiqvision@gmail.com>
2023-09-03 20:15:43 -0500

    add functionality for creating and closing broadcast channel

--- src/app/responsive-examples/line-size-analyzer/line-analyzer-input/line-analyzer-input.component.ts
+++ src/app/responsive-examples/line-size-analyzer/line-analyzer-input/line-analyzer-input.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LineAnalyzerSelectorComponent } from './line-analyzer-selector/line-analyzer-selector.component';
+import { LineAnalyzerInputService } from './line-analyzer-input.service';
 
 @Component({
-  selector: 'app-line-analyzer-input',
+  selector: 'line-analyzer-input',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LineAnalyzerSelectorComponent],
   templateUrl: './line-analyzer-input.component.html',
   styleUrls: ['./line-analyzer-input.component.css']
 })
-export class LineAnalyzerInputComponent {
+export class LineAnalyzerInputComponent implements OnInit, OnDestroy {
+
+  @Input() BroadcastName!: string;
+  @Input() ChannelName!: string;
+
+  constructor(private dataService: LineAnalyzerInputService){}
+
+  ngOnInit(): void {
+      this.dataService.createBroadcastChannel(this.BroadcastName, this.ChannelName);
+  }
+
+  ngOnDestroy(): void {
+      this.dataService.closeChannel();
+  }
 
 }
--- src/app/responsive-examples/line-size-analyzer/line-size-analyzer.component.ts
+++ src/app/responsive-examples/line-size-analyzer/line-size-analyzer.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LineSizeAnalyzerService } from './line-size-analyzer.service';
 
 @Component({
   selector: 'app-line-size-analyzer',
@@ -8,6 +9,19 @@ import { CommonModule } from '@angular/common';
   templateUrl: './line-size-analyzer.component.html',
   styleUrls: ['./line-size-analyzer.component.css']
 })
-export class LineSizeAnalyzerComponent {
+export class LineSizeAnalyzerComponent implements OnInit, OnDestroy {
+
+  @Input() BroadcastName!: string;
+  @Input() ChannelName!: string;
+
+  constructor(private dataService: LineSizeAnalyzerService){}
+
+  ngOnInit(): void {
+      this.dataService.createBroadcastChannel(this.BroadcastName, this.ChannelName);
+  }
+
+  ngOnDestroy(): void {
+      this.dataService.closeChannel();
+  }
 
 }
