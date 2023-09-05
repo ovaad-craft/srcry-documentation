1b62a4407283df9d192722e6e26a239c6a5cf97c
Optiq01 <Optiqvision@gmail.com>
2023-09-04 21:47:50 -0500

    create logic for updating sizes

--- src/app/responsive-examples/line-size-analyzer/line-analyzer-input/line-analyzer-selector/line-analyzer-selector.component.ts
+++ src/app/responsive-examples/line-size-analyzer/line-analyzer-input/line-analyzer-selector/line-analyzer-selector.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LineSizes } from '@site-types';
+import { LineAnalyzerSelectorService } from './line-analyzer-selector.service';
 
 @Component({
-  selector: 'app-line-analyzer-selector',
+  selector: 'line-analyzer-selector',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './line-analyzer-selector.component.html',
   styleUrls: ['./line-analyzer-selector.component.css']
 })
-export class LineAnalyzerSelectorComponent {
+export class LineAnalyzerSelectorComponent implements OnInit {
+
+  @Output() UpdateSize: EventEmitter<LineSizes> = new EventEmitter<LineSizes>();
+
+  Sizes!: LineSizes[];
+  CurrentSize: LineSizes = 'narrow';
+  SizeActive: boolean = false;
+
+  constructor(private dataService: LineAnalyzerSelectorService){}
+
+  ngOnInit(): void {
+      this.Sizes = this.dataService.getSizes();
+  }
+
+  public updateSize(size: LineSizes): void{
+    this.CurrentSize = size;
+    this.closeSizeList();
+  }
+
+  public openSizeList(): void{ this.SizeActive = true; }
+  public closeSizeList():void{ this.SizeActive = false; }
 
 }
