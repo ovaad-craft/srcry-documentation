46b4e291dc25db9c085b2bc2d394d444332c963c
Optiq01 <Optiqvision@gmail.com>
2023-09-06 21:44:24 -0500

    create prop chart component

---
+++ src/app/layout/prop-chart/prop-chart.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-prop-chart',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './prop-chart.component.html',
+  styleUrls: ['./prop-chart.component.css']
+})
+export class PropChartComponent {
+
+}
