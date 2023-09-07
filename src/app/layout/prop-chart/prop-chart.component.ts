859c7d24f38063c91d04408a68e295a44f8aec14
Optiq01 <Optiqvision@gmail.com>
2023-09-07 02:50:09 -0500

    markup, style and add input to prop chart

--- src/app/layout/prop-chart/prop-chart.component.ts
+++ src/app/layout/prop-chart/prop-chart.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PropChartData } from '@site-types';
 
 @Component({
-  selector: 'app-prop-chart',
+  selector: 'prop-chart',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './prop-chart.component.html',
   styleUrls: ['./prop-chart.component.css']
 })
 export class PropChartComponent {
+
+  @Input() ChartData!: PropChartData[];
 
 }
