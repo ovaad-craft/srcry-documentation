a212907c3fe45f401e5ffe16d64acda06f13b626
Optiq01 <Optiqvision@gmail.com>
2023-09-07 18:37:00 -0500

    create folder for base-size examples and first demonstration

---
+++ src/app/responsive-examples/base-size-examples/base-size-example-a/base-size-example-a.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-base-size-example-a',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './base-size-example-a.component.html',
+  styleUrls: ['./base-size-example-a.component.css']
+})
+export class BaseSizeExampleAComponent {
+
+}
