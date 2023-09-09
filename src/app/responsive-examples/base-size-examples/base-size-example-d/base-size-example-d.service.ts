b4b46a52591968eb2d4dd5f5b955791da70b899a
Optiq01 <Optiqvision@gmail.com>
2023-09-09 02:14:51 -0500

    create broadcast channel and runctionality for sending data

--- src/app/responsive-examples/base-size-examples/base-size-example-d/base-size-example-d.service.ts
+++ src/app/responsive-examples/base-size-examples/base-size-example-d/base-size-example-d.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+import { BaseSizeAnalyzerInterface } from '@site-types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseSizeExampleDService {
 
-  constructor() { }
+  DataChannel!: BroadcastChannel;
+  ComponentChannelName!: string;
+
+  constructor(private zone: NgZone) { }
+
+  public createChannel(broadcastName: string, componentChannelName: string):void{
+    this.DataChannel = new BroadcastChannel(broadcastName);
+    this.ComponentChannelName = componentChannelName;
+  }
+
+  public sendData(data: BaseSizeAnalyzerInterface):void{
+    this.zone.run(()=>{
+      this.DataChannel.postMessage({
+        target: this.ComponentChannelName,
+        payload: data
+      });
+    });
+  }
 }
