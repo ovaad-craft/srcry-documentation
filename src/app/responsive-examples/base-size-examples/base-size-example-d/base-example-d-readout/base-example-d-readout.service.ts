add499e24756e101dddd7bdf2d65b4627b161daf
Optiq01 <Optiqvision@gmail.com>
2023-09-09 02:27:34 -0500

    create broadcast channel and functionality for recieving data

--- src/app/responsive-examples/base-size-examples/base-size-example-d/base-example-d-readout/base-example-d-readout.service.ts
+++ src/app/responsive-examples/base-size-examples/base-size-example-d/base-example-d-readout/base-example-d-readout.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+import { BaseSizeAnalyzerInterface } from '@site-types';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseExampleDReadoutService {
 
-  constructor() { }
+  DataChannel!: BroadcastChannel;
+  ComponentChannelName!: string;
+
+  private Readings: BehaviorSubject<BaseSizeAnalyzerInterface> = new BehaviorSubject<BaseSizeAnalyzerInterface>({
+    fullWidth: 0,
+    baseWidth: 0,
+    chunkWidth: 0,
+    sliceWidth: 0
+  });
+
+  public Readings$ = this.Readings.asObservable();
+
+  constructor(private zone: NgZone) { }
+
+  private createChannelListener():void{
+    this.DataChannel.onmessage = (event)=>{
+      this.zone.run(()=>{
+        if(event.data.target === this.ComponentChannelName){
+          this.Readings.next(event.data.payload);
+        }
+      });
+    };
+  }
+
+  public createBroadcastChannel(broadcastName: string, componentChannelName: string): void{
+    this.DataChannel = new BroadcastChannel(broadcastName);
+    this.ComponentChannelName = componentChannelName;
+    this.createChannelListener();
+  }
 }
