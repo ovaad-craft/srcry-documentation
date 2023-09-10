e1a801eea85aa3142acba2adc44ccbb2de186298
Optiq01 <Optiqvision@gmail.com>
2023-09-09 22:04:18 -0500

    subscribe to readings data and add closeChannel function to onDestroy hook

--- src/app/responsive-examples/base-size-examples/base-size-example-d/base-example-d-readout/base-example-d-readout.component.ts
+++ src/app/responsive-examples/base-size-examples/base-size-example-d/base-example-d-readout/base-example-d-readout.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BaseSizeExampleDService } from '../base-size-example-d.service';
+import { BaseSizeAnalyzerInterface } from '@site-types';
+import { BaseExampleDReadoutService } from './base-example-d-readout.service';
 
 @Component({
   selector: 'app-base-example-d-readout',
@@ -8,6 +11,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './base-example-d-readout.component.html',
   styleUrls: ['./base-example-d-readout.component.css']
 })
-export class BaseExampleDReadoutComponent {
+export class BaseExampleDReadoutComponent implements OnInit, OnDestroy {
+
+  Readings!: BaseSizeAnalyzerInterface;
+
+  constructor(private dataService: BaseExampleDReadoutService){}
+
+  ngOnInit(): void {
+      this.dataService.Readings$.subscribe(a=> this.Readings = a);
+  }
+
+  ngOnDestroy(): void {
+      this.dataService.closeChannel();
+  }
 
 }
--- src/app/responsive-examples/base-size-examples/base-size-example-d/base-size-example-d.component.ts
+++ src/app/responsive-examples/base-size-examples/base-size-example-d/base-size-example-d.component.ts
@@ -1,6 +1,7 @@
-import { AfterContentChecked, AfterViewInit, Component, Input, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterContentChecked, AfterViewInit, Component, Input, ElementRef, NgZone, ViewChild, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BaseSizeExampleDService } from './base-size-example-d.service';
+import { BaseSizeAnalyzerInterface } from '@site-types';
 
 @Component({
   selector: 'app-base-size-example-d',
@@ -15,22 +16,41 @@ export class BaseSizeExampleDComponent implements OnInit, AfterViewInit, AfterCo
   @Input()BroadcastName!: string;
   @Input()ChannelName!: string;
 
-  constructor(private dataChannel: BaseSizeExampleDService){}
+  @ViewChild('fullBox', {static: true, read: ElementRef})FullBox!: ElementRef;
+  @ViewChild('baseBox', {static: true, read: ElementRef})BaseBox!: ElementRef;
+  @ViewChild('chunkBox', {static: true, read: ElementRef})ChunkBox!: ElementRef;
+
+  constructor(private zone: NgZone, private dataChannel: BaseSizeExampleDService){}
 
   ngOnInit(): void {
     this.dataChannel.createChannel(this.BroadcastName, this.ChannelName);
   }
 
   ngAfterViewInit(): void {
-      
+    const frame: ResizeObserver = new ResizeObserver((item)=>{
+      this.zone.run(()=>{
+        this.readElements();
+      });
+    });
+
+    frame.observe(this.FullBox.nativeElement);      
   }
 
   ngAfterContentChecked(): void {
-      
+    this.readElements();
   }
 
   ngOnDestroy(): void {
       this.dataChannel.closeChannel();
+  }
+
+  private readElements():void{
+    const sizes: BaseSizeAnalyzerInterface = {
+      baseWidth: this.BaseBox.nativeElement.offsetWidth,
+      chunkWidth: this.ChunkBox.nativeElement.offsetWidth
+    };
+
+    this.dataChannel.sendData(sizes);
   }
 
 }
