69c8de8e3643173960c6c383d4a22f860f46f47b
Optiq01 <Optiqvision@gmail.com>
2023-09-13 18:59:29 -0500

    add input for Target name to functionality

--- src/app/responsive-examples/base-size-examples/base-size-example-c/base-size-c-readout/base-size-c-readout.component.ts
+++ src/app/responsive-examples/base-size-examples/base-size-example-c/base-size-c-readout/base-size-c-readout.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BaseSizeCReadoutService } from './base-size-c-readout.service';
+import { BaseSizeAnalyzerInterface } from '@site-types';
 
 @Component({
   selector: 'base-size-c-readout',
@@ -8,6 +10,23 @@ import { CommonModule } from '@angular/common';
   templateUrl: './base-size-c-readout.component.html',
   styleUrls: ['./base-size-c-readout.component.css']
 })
-export class BaseSizeCReadoutComponent {
+export class BaseSizeCReadoutComponent implements OnInit, OnDestroy {
+
+  @Input()BroadcastName!: string;
+  @Input()ChannelName!: string;
+  @Input()TargetName!: string;
+
+  Readings!: BaseSizeAnalyzerInterface;
+
+  constructor(private dataChannel: BaseSizeCReadoutService){}
+
+  ngOnInit(): void {
+    this.dataChannel.createBroadcastChannel(this.BroadcastName, this.ChannelName, this.TargetName);
+    this.dataChannel.Readings$.subscribe(a=> this.Readings = a);
+  }
+
+  ngOnDestroy(): void {
+    this.dataChannel.closeChannel();
+  }
 
 }
--- src/app/responsive-examples/base-size-examples/base-size-example-c/base-size-c-readout/base-size-c-readout.service.ts
+++ src/app/responsive-examples/base-size-examples/base-size-example-c/base-size-c-readout/base-size-c-readout.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+import { BaseSizeAnalyzerInterface } from '@site-types';
+import {BehaviorSubject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseSizeCReadoutService {
 
-  constructor() { }
+  private DataChannel!: BroadcastChannel;
+  private ComponentChannelName!: string;
+  private TargetName!: string;
+
+  private Readings: BehaviorSubject<BaseSizeAnalyzerInterface> = new BehaviorSubject<BaseSizeAnalyzerInterface>({
+    fullWidth: 0,
+    baseWidth: 0,
+    chunkWidth: 0,
+    sliceWidth: 0
+  });
+
+  public Readings$ = this.Readings.asObservable();
+
+  constructor(private zone: NgZone) { }
+
+  private createChannelListener():void{
+    this.DataChannel.onmessage = ((event)=>{
+      this.zone.run(()=>{
+        if(event.data.target === this.ComponentChannelName){
+          this.Readings.next(event.data.payload);
+        }
+      });
+    });
+  }
+
+  public createBroadcastChannel(broadcastName: string, channelName: string, targetName: string): void{
+    this.DataChannel = new BroadcastChannel(broadcastName);
+    this.ComponentChannelName = channelName;
+    this.TargetName = targetName;
+    this.createChannelListener();
+  }
+
+  public closeChannel():void{
+    this.DataChannel.close();
+  }
 }
