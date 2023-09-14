2bf7e6add93bdf5807e6d5c8e651cd3cc9edcc1f
Optiq01 <Optiqvision@gmail.com>
2023-09-13 23:21:54 -0500

    add functionality for closing broadcast channel

--- src/app/responsive-examples/base-size-examples/base-size-analyzer/base-size-analyzer-controller/base-size-analyzer-controller.component.ts
+++ src/app/responsive-examples/base-size-examples/base-size-analyzer/base-size-analyzer-controller/base-size-analyzer-controller.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BaseSizeAnalyzerControllerService } from './base-size-analyzer-controller.service';
+import { BaseSizeProps, BaseSizeValues } from '@site-types';
 
 @Component({
-  selector: 'app-base-size-analyzer-controller',
+  selector: 'base-size-analyzer-controller',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './base-size-analyzer-controller.component.html',
   styleUrls: ['./base-size-analyzer-controller.component.css']
 })
-export class BaseSizeAnalyzerControllerComponent {
+export class BaseSizeAnalyzerControllerComponent implements OnInit, OnDestroy {
+  @Input()BroadcastName!: string;
+  @Input()ChannelName!: string;
+  @Input()TargetName!: string;
 
+  Values!: BaseSizeProps;
+  Readings!: BaseSizeValues;
+
+  constructor(private dataService: BaseSizeAnalyzerControllerService){}
+
+  ngOnInit(): void {
+    this.dataService.Readings$.subscribe(a => this.Readings = a);
+  }
+
+  ngOnDestroy(): void {
+      this.dataService.closeChannel();
+  }
 }
--- src/app/responsive-examples/base-size-examples/base-size-analyzer/base-size-analyzer-controller/base-size-analyzer-controller.service.ts
+++ src/app/responsive-examples/base-size-examples/base-size-analyzer/base-size-analyzer-controller/base-size-analyzer-controller.service.ts
@@ -1,9 +1,62 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+import { BaseSizeProps, BaseSizeValues } from '@site-types';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseSizeAnalyzerControllerService {
 
-  constructor() { }
+  DataChannel!: BroadcastChannel;
+  ChannelName!: string;
+  TargetName!: string;
+
+  DefaultSettings: BaseSizeProps = {
+    baseSizeW: 'xTiny-1-5',
+    baseSizeWNudgeChunk: 0,
+    baseSizeWNudgeSlice: 0,
+    baseSizeH: 'tiny-1-5',
+    baseSizeHNudgeChunk: 0,
+    baseSizeHNudgeSlice: 0
+  };
+  private Readings: BehaviorSubject<BaseSizeValues> = new BehaviorSubject<BaseSizeValues>({
+    width: 0, height: 0
+  });
+
+  public Readings$ = this.Readings.asObservable();
+
+  constructor(private zone: NgZone) { }
+
+  private createChannelListener():void{
+    this.DataChannel.onmessage = (event)=>{
+      this.zone.run(()=>{
+        if(event.data.target === this.ChannelName  && event.data.notification === 'data'){
+          this.Readings.next(event.data.payload);
+        }
+        if(event.data.target === this.ChannelName && event.data.notification === 'loadComplete'){
+          this.sendData(this.DefaultSettings);
+        }
+      });
+    };
+  }
+
+  public createChannel(broadcastName: string, channelName: string, targetName: string): void{
+    this.DataChannel = new BroadcastChannel(broadcastName);
+    this.ChannelName = channelName;
+    this.TargetName = targetName;
+    this.createChannelListener();
+  }
+
+  public sendData(data: BaseSizeProps):void{
+    this.zone.run(()=>{
+      this.DataChannel.postMessage({
+        target: this.TargetName,
+        payload: data
+      });
+    });
+  }
+
+  public closeChannel():void{
+    this.DataChannel.close();
+  }
 }
