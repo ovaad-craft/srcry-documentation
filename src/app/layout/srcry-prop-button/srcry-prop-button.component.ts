57cb347a49e4b7f1d3cb63a97d39ddca93d9cfbb
Optiq01 <Optiqvision@gmail.com>
2023-09-14 18:33:14 -0500

    create srcry prop button

---
+++ src/app/layout/srcry-prop-button/srcry-prop-button.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-srcry-prop-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './srcry-prop-button.component.html',
+  styleUrls: ['./srcry-prop-button.component.css']
+})
+export class SrcryPropButtonComponent {
+
+}
