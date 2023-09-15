bc715cbbdd203e2e7d3715c7f059592b1c77f515
Optiq01 <Optiqvision@gmail.com>
2023-09-15 01:46:00 -0500

    create number input

---
+++ src/app/layout/number-input/number-input.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'number-input',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './number-input.component.html',
+  styleUrls: ['./number-input.component.css']
+})
+export class NumberInputComponent {
+
+}
