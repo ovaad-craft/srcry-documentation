54bcdc86d9df204d470f17e0d86cbc42a10b28eb
Optiq01 <Optiqvision@gmail.com>
2023-09-16 01:20:10 -0500

    create default settings for baseSize controller

--- src/app/responsive-examples/base-size-examples/base-size-analyzer/base-size-analyzer-controller/base-size-analyzer-controller.service.ts
+++ src/app/responsive-examples/base-size-examples/base-size-analyzer/base-size-analyzer-controller/base-size-analyzer-controller.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NgZone } from '@angular/core';
-import { BaseSizeProps, BaseSizeValues } from '@site-types';
+import { BaseSizePropData, BaseSizeProps, BaseSizeValues } from '@site-types';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
@@ -19,6 +19,16 @@ export class BaseSizeAnalyzerControllerService {
     baseSizeHNudgeChunk: 0,
     baseSizeHNudgeSlice: 0
   };
+
+  DefaultControlSettings: BaseSizePropData = {
+    baseSizeW: {size: 'xTiny', scale: '1', speed: '5'},
+    baseSizeWNudgeChunk: 0,
+    baseSizeWNudgeSlice: 0,
+    baseSizeH: {size: 'tiny', scale: '1', speed: '5'},
+    baseSizeHNudgeChunk: 0,
+    baseSizeHNudgeSlice: 0
+  }
+
   private Readings: BehaviorSubject<BaseSizeValues> = new BehaviorSubject<BaseSizeValues>({
     width: 0, height: 0
   });
@@ -54,6 +64,10 @@ export class BaseSizeAnalyzerControllerService {
         payload: data
       });
     });
+  }
+
+  public getDefaultSettings(): BaseSizePropData{
+    return this.DefaultControlSettings;
   }
 
   public closeChannel():void{
