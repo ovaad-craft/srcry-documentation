b0a4dce46ecbe6da14f6b3fd1901ef4d302d0f49
Optiq01 <Optiqvision@gmail.com>
2023-09-16 01:16:01 -0500

    create input for numerical values

--- src/app/layout/number-input/number-input.component.ts
+++ src/app/layout/number-input/number-input.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'number-input',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './number-input.component.html',
   styleUrls: ['./number-input.component.css']
 })
-export class NumberInputComponent {
+export class NumberInputComponent implements OnInit {
 
+  @Input()DefaultSetting?: number;
+
+  @Output()UpdateSize: EventEmitter<number> = new EventEmitter<number>();
+
+  Value: number = 0;
+
+  ngOnInit(): void {
+      if(this.DefaultSetting){
+        this.Value = this.DefaultSetting;
+      }
+  }
+
+  public incramentUp():void{
+    this.Value++;
+    this.updateSize();
+  }
+
+  public incramentDown():void{
+    this.Value--;
+    this.updateSize();
+  }
+
+  private updateSize():void{
+    this.UpdateSize.emit(this.Value);
+  }
 }
