45039095825c2a2c21c29a89bd95d8c3d8ed7dd4
Optiq01 <Optiqvision@gmail.com>
2023-09-16 01:03:23 -0500

    add functionality for updating props and indicate if a selection frame is active

--- src/app/responsive-examples/box-size-examples/box-size-analyzer/analyzer-input/box-size-selector/box-size-selector.component.ts
+++ src/app/responsive-examples/box-size-examples/box-size-analyzer/analyzer-input/box-size-selector/box-size-selector.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BoxSizeInterface, BoxSizeScale, BoxSizeSize, BoxSizeSpeed } from '@site-types';
+import { BaseSizePropData, BoxSizeInterface, BoxSizeScale, BoxSizeSize, BoxSizeSpeed } from '@site-types';
 import { BoxSizeSelectorService } from './box-size-selector.service';
 import { DropdownAnimation } from 'src/app/layout/sidebar/link/link.component';
 
@@ -14,7 +14,9 @@ import { DropdownAnimation } from 'src/app/layout/sidebar/link/link.component';
 })
 export class BoxSizeSelectorComponent implements OnInit {
 
+  @Input() DefaultSettings?: BoxSizeInterface;
   @Output() UpdateSelection : EventEmitter<BoxSizeInterface> = new EventEmitter<BoxSizeInterface>();
+  @Output() UpdateStatus: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   BoxSizeSizes!: BoxSizeSize[];
   BoxSizeScales!: BoxSizeScale[];
@@ -37,6 +39,12 @@ export class BoxSizeSelectorComponent implements OnInit {
       this.BoxSizeScales = this.dataService.getScales();
       this.BoxSizeSpeeds = this.dataService.getSpeeds();
 
+      if(this.DefaultSettings){
+        this.SelectedSize = this.DefaultSettings.size;
+        this.SelectedScale = this.DefaultSettings.scale;
+        this.SelectedSpeed = this.DefaultSettings.speed;
+      }
+
       this.UpdateSelection.emit({
         size: this.SelectedSize,
         scale: this.SelectedScale,
@@ -51,12 +59,14 @@ export class BoxSizeSelectorComponent implements OnInit {
       this.SizeToggle = false;
       this.ScaleToggle = false;
       this.SpeedToggle = false;
+      this.UpdateStatus.emit(false);
     }
     else{
       this.ActiveSelector = selector;
       if(selector === 'size'){ this.SizeToggle = true; }
       if(selector === 'scale'){ this.ScaleToggle = true; }
       if(selector === 'speed'){ this.SpeedToggle = true; }
+      this.UpdateStatus.emit(true);
     }
 
   }
@@ -65,33 +75,31 @@ export class BoxSizeSelectorComponent implements OnInit {
     this.SelectedSize = size;
     this.ActiveSelector = 'none';
     this.SizeToggle = false;
-    this.UpdateSelection.emit({
-      size: this.SelectedSize,
-      scale: this.SelectedScale,
-      speed: this.SelectedSpeed
-    });
+    this.updateSelection();
   }
   
   public updateScale(scale: BoxSizeScale):void{
     this.SelectedScale = scale;
     this.ActiveSelector = 'none';
     this.ScaleToggle = false;
-    this.UpdateSelection.emit({
-      size: this.SelectedSize,
-      scale: this.SelectedScale,
-      speed: this.SelectedSpeed
-    });
+    this.updateSelection();
   }
   
   public updateSpeed(speed: BoxSizeSpeed):void{
     this.SelectedSpeed = speed;
     this.ActiveSelector = 'none';
     this.SpeedToggle = false;
+    this.updateSelection();
+  }
+
+  private updateSelection(): void{
     this.UpdateSelection.emit({
       size: this.SelectedSize,
       scale: this.SelectedScale,
       speed: this.SelectedSpeed
     });
+
+    this.UpdateStatus.emit(false);
   }
 
 }
