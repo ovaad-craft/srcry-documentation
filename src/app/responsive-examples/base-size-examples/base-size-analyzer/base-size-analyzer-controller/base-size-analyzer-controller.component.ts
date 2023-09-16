f124857ef775bc2ad63fb372295ab1d1c833b0ec
Optiq01 <Optiqvision@gmail.com>
2023-09-16 01:21:49 -0500

    call default data from service

--- src/app/responsive-examples/base-size-examples/base-size-analyzer/base-size-analyzer-controller/base-size-analyzer-controller.component.ts
+++ src/app/responsive-examples/base-size-examples/base-size-analyzer/base-size-analyzer-controller/base-size-analyzer-controller.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BaseSizeAnalyzerControllerService } from './base-size-analyzer-controller.service';
-import { BaseSizeProps, BaseSizeValues } from '@site-types';
+import { BaseSizePropData, BaseSizeProps, BaseSizeSettings, BaseSizeValues } from '@site-types';
+import { BaseSizeInputComponent } from './base-size-input/base-size-input.component';
 
 @Component({
   selector: 'base-size-analyzer-controller',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BaseSizeInputComponent],
   templateUrl: './base-size-analyzer-controller.component.html',
   styleUrls: ['./base-size-analyzer-controller.component.css']
 })
@@ -18,13 +19,21 @@ export class BaseSizeAnalyzerControllerComponent implements OnInit, OnDestroy {
   Values!: BaseSizeProps;
   Readings!: BaseSizeValues;
 
+  DefaultSettings!: BaseSizePropData;
+
   constructor(private dataService: BaseSizeAnalyzerControllerService){}
 
   ngOnInit(): void {
     this.dataService.Readings$.subscribe(a => this.Readings = a);
+    this.DefaultSettings = this.dataService.getDefaultSettings();
+    this.dataService.createChannel(this.BroadcastName, this.ChannelName, this.TargetName);
   }
 
   ngOnDestroy(): void {
       this.dataService.closeChannel();
+  }
+
+  public updateValues(value: BaseSizeProps):void{
+    this.dataService.sendData(value);
   }
 }
