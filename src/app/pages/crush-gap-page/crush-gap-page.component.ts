811da565c65389afe93e3f5361b3eae2cf88a5d7
Optiq01 <Optiqvision@gmail.com>
2023-09-16 22:09:31 -0500

    import pagination component and create pagination data

--- src/app/pages/crush-gap-page/crush-gap-page.component.ts
+++ src/app/pages/crush-gap-page/crush-gap-page.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PaginateComponent } from 'src/app/layout/paginate/paginate.component';
+import { PaginationData } from '@site-types';
+
+const PAGINATIONDATA: PaginationData = {
+  previous:{
+    id: 'baseSizePage',
+    title: 'Base Size',
+    path: '/bse-size'
+  },
+  next:{
+    id: 'edgeChase',
+    title: 'Edge Chase',
+    path: '/edge-chase'
+  }
+}
 
 @Component({
   selector: 'app-crush-gap-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PaginateComponent],
   templateUrl: './crush-gap-page.component.html',
   styleUrls: ['./crush-gap-page.component.css']
 })
 export class CrushGapPageComponent {
+
+  Pagination: PaginationData = PAGINATIONDATA;
 
 }
