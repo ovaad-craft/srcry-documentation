55fc5ecf0636c12bbe5253c6e496c2c8c4eab19b
Optiq01 <Optiqvision@gmail.com>
2023-09-17 01:20:42 -0500

    craete crush-gap-example-a component

---
+++ src/app/responsive-examples/crush-gap-examples/crush-gap-example-a/crush-gap-example-a.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-crush-gap-example-a',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './crush-gap-example-a.component.html',
+  styleUrls: ['./crush-gap-example-a.component.css']
+})
+export class CrushGapExampleAComponent {
+
+}
