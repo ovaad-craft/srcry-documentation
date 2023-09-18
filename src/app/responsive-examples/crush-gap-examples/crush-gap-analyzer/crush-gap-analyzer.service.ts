cf32b0d39d32db4096c956aa616e6662e2472e1d
Optiq01 <Optiqvision@gmail.com>
2023-09-18 01:00:58 -0500

    make function for creating broadcast channel and setting channel listener

--- src/app/responsive-examples/crush-gap-examples/crush-gap-analyzer/crush-gap-analyzer.service.ts
+++ src/app/responsive-examples/crush-gap-examples/crush-gap-analyzer/crush-gap-analyzer.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+import { CrushGapProps } from '@site-types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrushGapAnalyzerService {
 
-  constructor() { }
+  DataChannel! : BroadcastChannel;
+  ChannelName! : string;
+  TargetName! : string;
+
+  constructor(private zone: NgZone) { }
+
+  private setChannelListener():void{
+    this.DataChannel.onmessage = (event)=>{
+      this.zone.run(()=>{
+        if(event.data.target === this.ChannelName){
+          this.updateData(event.data.payload);
+        }
+      });
+    };
+  }
+
+  public createBroadcastChannel(broadcastName: string, channelName: string, targetName: string): void{
+    this.DataChannel = new BroadcastChannel(broadcastName);
+    this.ChannelName = channelName;
+    this.TargetName = targetName;
+    this.setChannelListener();
+  }
+
+  private updateData(data: CrushGapProps):void{}
 }
