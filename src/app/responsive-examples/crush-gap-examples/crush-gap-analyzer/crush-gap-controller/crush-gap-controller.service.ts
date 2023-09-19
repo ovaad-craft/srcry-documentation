f65e95c4449fdc5b12f100319ffa03199b65f6e4
Optiq01 <Optiqvision@gmail.com>
2023-09-19 01:34:43 -0500

    create naming variables and defalt settings

--- src/app/responsive-examples/crush-gap-examples/crush-gap-analyzer/crush-gap-controller/crush-gap-controller.service.ts
+++ src/app/responsive-examples/crush-gap-examples/crush-gap-analyzer/crush-gap-controller/crush-gap-controller.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+import { CrushGapPropData } from '@site-types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrushGapControllerService {
 
-  constructor() { }
+  constructor(private zone: NgZone) { }
+
+  DataChannel!: BroadcastChannel;
+  ChannelName!: string;
+  TargetName!: string;
+
+  DefaultSettings: CrushGapPropData = {
+    crushGapW: {size:'xTiny', scale: '1', speed: '4'},
+    crushGapWNudgeChunk: 0,
+    crushGapWNudgeSlice: 0,
+    crushGapH: {size:'xTiny', scale: '2', speed: '4'},
+    crushGapHNudgeChunk: 0,
+    crushGapHNudgeSlice: 0
+  }
 }
