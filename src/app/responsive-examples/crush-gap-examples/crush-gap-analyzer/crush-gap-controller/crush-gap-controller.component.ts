15662b72a30f1992e560959b8f4c5bfdd110d1c9
Optiq01 <Optiqvision@gmail.com>
2023-09-19 02:58:01 -0500

    subscribe to readings and trigger createBroadcastChannel function

--- src/app/responsive-examples/crush-gap-examples/crush-gap-analyzer/crush-gap-controller/crush-gap-controller.component.ts
+++ src/app/responsive-examples/crush-gap-examples/crush-gap-analyzer/crush-gap-controller/crush-gap-controller.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, NgZone, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CrushGapInputComponent } from './crush-gap-input/crush-gap-input.component';
+import { CrushGapControllerService } from './crush-gap-controller.service';
+import { CrushGapReadings } from '@site-types';
 
 @Component({
   selector: 'crush-gap-controller',
@@ -9,10 +11,19 @@ import { CrushGapInputComponent } from './crush-gap-input/crush-gap-input.compon
   templateUrl: './crush-gap-controller.component.html',
   styleUrls: ['./crush-gap-controller.component.css']
 })
-export class CrushGapControllerComponent {
+export class CrushGapControllerComponent implements OnInit {
 
   @Input()BroadcastName!: string;
   @Input()ChannelName!: string;
   @Input()TargetName!: string;
+
+  Readings!: CrushGapReadings;
+
+  constructor(private dataService: CrushGapControllerService){}
+
+  ngOnInit(): void {
+    this.dataService.Readings$.subscribe(a=> this.Readings = a);
+    this.dataService.createBroadcastChannel(this.BroadcastName, this.ChannelName, this.TargetName);
+  }
 
 }
