09aa0ca0d794c37a5d14a261f3f484fc9826d17d
Optiq01 <Optiqvision@gmail.com>
2023-09-19 02:37:37 -0500

    create functionality for creating broadcast channel and sending default data

--- src/app/responsive-examples/crush-gap-examples/crush-gap-analyzer/crush-gap-controller/crush-gap-controller.service.ts
+++ src/app/responsive-examples/crush-gap-examples/crush-gap-analyzer/crush-gap-controller/crush-gap-controller.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, NgZone } from '@angular/core';
-import { CrushGapPropData } from '@site-types';
+import { CrushGapPropData, CrushGapSettings } from '@site-types';
+import { createBoxSize } from 'src/app/utils/create-box-size';
+import { createCssVariable } from 'src/app/utils/create-css-variable';
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +21,48 @@ export class CrushGapControllerService {
     crushGapH: {size:'xTiny', scale: '2', speed: '4'},
     crushGapHNudgeChunk: 0,
     crushGapHNudgeSlice: 0
+  };
+
+
+  private createChannelListener():void{
+    this.DataChannel.onmessage = (event)=>{
+      this.zone.run(()=>{
+        if(event.data.notification === 'loadComplete'){
+          this.sendData({
+            crushGapW: createCssVariable(createBoxSize(
+              this.DefaultSettings.crushGapW.size,
+              this.DefaultSettings.crushGapW.scale,
+              this.DefaultSettings.crushGapW.speed
+            )),
+            crushGapWNudgeChunk: this.DefaultSettings.crushGapWNudgeChunk,
+            crushGapWNudgeSlice: this.DefaultSettings.crushGapWNudgeSlice,
+            crushGapH: createCssVariable(createBoxSize(
+              this.DefaultSettings.crushGapH.size,
+              this.DefaultSettings.crushGapH.scale,
+              this.DefaultSettings.crushGapH.speed
+            )),
+            crushGapHNudgeChunk: this.DefaultSettings.crushGapHNudgeChunk,
+            crushGapHNudgeSlice: this.DefaultSettings.crushGapHNudgeSlice
+          });
+        }
+        if(event.data.notification === 'data'){}
+      });
+    };
+  }
+
+  public createBroadcastChannel(broadcastName: string, channelName: string, targetName: string):void{
+    this.DataChannel = new BroadcastChannel(broadcastName);
+    this.ChannelName = channelName;
+    this.TargetName = targetName;
+    this.createChannelListener();
+  }
+
+  public sendData(data: CrushGapSettings):void{
+    this.zone.run(()=>{
+      this.DataChannel.postMessage({
+        target: this.TargetName,
+        payload: data
+      });
+    });
   }
 }
