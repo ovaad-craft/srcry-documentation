c717e7abc9351663ce60585b7be30ee59a28a911
Optiq01 <Optiqvision@gmail.com>
2023-09-22 00:16:54 -0500

    import box size selector, number input and srcry prop button components

--- src/app/responsive-examples/crush-gap-examples/crush-gap-analyzer/crush-gap-controller/crush-gap-input/crush-gap-input.component.ts
+++ src/app/responsive-examples/crush-gap-examples/crush-gap-analyzer/crush-gap-controller/crush-gap-input/crush-gap-input.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BoxSizeSelectorComponent } from 'src/app/responsive-examples/box-size-examples/box-size-analyzer/analyzer-input/box-size-selector/box-size-selector.component';
+import { NumberInputComponent } from 'src/app/layout/number-input/number-input.component';
+import { SrcryPropButtonComponent } from 'src/app/layout/srcry-prop-button/srcry-prop-button.component';
 
 @Component({
-  selector: 'app-crush-gap-input',
+  selector: 'crush-gap-input',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    BoxSizeSelectorComponent,
+    NumberInputComponent,
+    SrcryPropButtonComponent
+  ],
   templateUrl: './crush-gap-input.component.html',
   styleUrls: ['./crush-gap-input.component.css']
 })
