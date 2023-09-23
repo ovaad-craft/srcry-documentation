da5159bf3f28ae6d86cd72514dbba19eddb8a4c0
Optiq01 <Optiqvision@gmail.com>
2023-09-23 00:19:26 -0500

    create edge chase example a

---
+++ src/app/responsive-examples/edge-chase-examples/edge-chase-example-a/edge-chase-example-a.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-edge-chase-example-a',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './edge-chase-example-a.component.html',
+  styleUrls: ['./edge-chase-example-a.component.css']
+})
+export class EdgeChaseExampleAComponent {
+
+}
