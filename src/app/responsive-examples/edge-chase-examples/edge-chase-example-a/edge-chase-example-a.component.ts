e1d845d15d8ae4c5749341ccf55f91801782f100
Optiq01 <Optiqvision@gmail.com>
2023-09-23 00:27:24 -0500

    make encapsulation use shadow dom

--- src/app/responsive-examples/edge-chase-examples/edge-chase-example-a/edge-chase-example-a.component.ts
+++ src/app/responsive-examples/edge-chase-examples/edge-chase-example-a/edge-chase-example-a.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-edge-chase-example-a',
+  selector: 'edge-chase-example-a',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './edge-chase-example-a.component.html',
-  styleUrls: ['./edge-chase-example-a.component.css']
+  styleUrls: ['./edge-chase-example-a.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class EdgeChaseExampleAComponent {
 
