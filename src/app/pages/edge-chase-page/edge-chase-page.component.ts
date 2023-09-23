10c6e277a60ee70d7a94610780e46afdc88ffa87
Optiq01 <Optiqvision@gmail.com>
2023-09-22 20:41:43 -0500

    import paginate component, demonstration frame and code display component

--- src/app/pages/edge-chase-page/edge-chase-page.component.ts
+++ src/app/pages/edge-chase-page/edge-chase-page.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PaginateComponent } from 'src/app/layout/paginate/paginate.component';
+import { DemonstrationFrameComponent } from 'src/app/layout/demonstration-frame/demonstration-frame.component';
+import { CodeDisplayComponent } from 'src/app/layout/code-display/code-display.component';
 
 @Component({
   selector: 'app-edge-chase-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    PaginateComponent,
+    DemonstrationFrameComponent,
+    CodeDisplayComponent
+  ],
   templateUrl: './edge-chase-page.component.html',
   styleUrls: ['./edge-chase-page.component.css']
 })
