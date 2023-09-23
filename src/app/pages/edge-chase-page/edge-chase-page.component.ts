8e9ecf4a387fe221336249004d607a0b1e8c3ca5
Optiq01 <Optiqvision@gmail.com>
2023-09-23 00:26:01 -0500

    import edgeChase example a and pass into demonstration frame

--- src/app/pages/edge-chase-page/edge-chase-page.component.ts
+++ src/app/pages/edge-chase-page/edge-chase-page.component.ts
@@ -5,6 +5,7 @@ import { DemonstrationFrameComponent } from 'src/app/layout/demonstration-frame/
 import { CodeDisplayComponent } from 'src/app/layout/code-display/code-display.component';
 import { PaginationData, PropChartData } from '@site-types';
 import { PropChartComponent } from 'src/app/layout/prop-chart/prop-chart.component';
+import { EdgeChaseExampleAComponent } from 'src/app/responsive-examples/edge-chase-examples/edge-chase-example-a/edge-chase-example-a.component';
 
 const PAGINATIONDATA: PaginationData = {
   previous: {
@@ -27,27 +28,27 @@ const PROPCHART: PropChartData[] = [
   },
   {
     id: 'edgeChaseWNudgeChunk',
-    title: 'edgeChase-w-nudge-chunk',
+    title: '--edgeChase-w-nudge-chunk',
     description: 'Defines the amount of nudge chunks to add to the --edgeChase-w property.'
   },
   {
     id: 'edgeChaseWNudgeSlice',
-    title: 'edgeChase-w-nudge-slice',
+    title: '--edgeChase-w-nudge-slice',
     description: 'Defines the amount of nudge slices to add to the --edgeChase-w property.'
   },
   {
     id: 'chaseStop-w',
-    title: 'chaseStop-w',
+    title: '--chaseStop-w',
     description: 'Defines the maximum width your element can expand to before it stops preserving the white space defined in the --edgeChase-w property.'
   },
   {
     id: 'chaseStopWNudgeChunk',
-    title: 'chaseStop-w-nudge-chunk',
+    title: '--chaseStop-w-nudge-chunk',
     description: 'Defines the amount of nudge chunks to add to the --chaseStop-w property.'
   },
   {
     id: 'chaseStopWNudgeSlice',
-    title: 'chaseStop-w-nudge-slice',
+    title: '--chaseStop-w-nudge-slice',
     description: 'Defines the amount of nudge slices to add to the --chaseStop-w property.'
   },
   {
@@ -57,27 +58,27 @@ const PROPCHART: PropChartData[] = [
   },
   {
     id: 'edgeChaseHNudgeChunk',
-    title: 'edgeChase-h-nudge-chunk',
+    title: '--edgeChase-h-nudge-chunk',
     description: 'Defines the amount of nudge chunks to add to the --edgeChase-h property.'
   },
   {
     id: 'edgeChaseHNudgeSlice',
-    title: 'edgeChase-h-nudge-slice',
+    title: '--edgeChase-h-nudge-slice',
     description: 'Defines the amount of nudge slices to add to the --edgeChase-h property.'
   },
   {
     id: 'chaseStop-w',
-    title: 'chaseStop-h',
+    title: '--chaseStop-h',
     description: 'Defines the maximum height your element can expand to before it stops preserving the white space defined in the --edgeChase-h property.'
   },
   {
     id: 'chaseStopHNudgeChunk',
-    title: 'chaseStop-h-nudge-chunk',
+    title: '--chaseStop-h-nudge-chunk',
     description: 'Defines the amount of nudge chunks to add to the --chaseStop-h property.'
   },
   {
     id: 'chaseStopHNudgeSlice',
-    title: 'chaseStop-h-nudge-slice',
+    title: '--chaseStop-h-nudge-slice',
     description: 'Defines the amount of nudge slices to add to the --chaseStop-h property.'
   }
 ];
@@ -99,5 +100,19 @@ export class EdgeChasePageComponent {
 
   Pagination: PaginationData = PAGINATIONDATA;
   PropData: PropChartData[] = PROPCHART;
+
+  Demo_01: string = ` .myClass{
+    --baseSize-w: var(--tiny-1-4);
+    --crushGap-w: var(--line-narrow);
+    --edgeChase-w: var(--small-2-3);
+    --chaseStop-w: var(--loMed-2-4);
+    --baseSize-h: var(--tiny-2-4);
+    --crushGap-h: var(--line-narrow);
+    --edgeChase-h: var(--tiny-1-3);
+    --chaseStop-h: var(--loMed-1-3);
+  }
+ `;
+
+ DemoComponent_01 = EdgeChaseExampleAComponent;
 
 }
