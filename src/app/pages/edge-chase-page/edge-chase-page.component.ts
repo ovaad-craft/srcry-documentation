d4b33e8b9ec3d5a6c180fbba6cdbc27f70acf886
Optiq01 <Optiqvision@gmail.com>
2023-09-22 20:49:43 -0500

    create pagination data and add paginate component to template

--- src/app/pages/edge-chase-page/edge-chase-page.component.ts
+++ src/app/pages/edge-chase-page/edge-chase-page.component.ts
@@ -3,6 +3,20 @@ import { CommonModule } from '@angular/common';
 import { PaginateComponent } from 'src/app/layout/paginate/paginate.component';
 import { DemonstrationFrameComponent } from 'src/app/layout/demonstration-frame/demonstration-frame.component';
 import { CodeDisplayComponent } from 'src/app/layout/code-display/code-display.component';
+import { PaginationData } from '@site-types';
+
+const PAGINATIONDATA: PaginationData = {
+  previous: {
+    id: 'crushGapPage',
+    title: 'Crush Gap',
+    path: '/crush-gap'
+  },
+  next: {
+    id: 'squishGrowthPage',
+    title: 'Squish Growth',
+    path: '/squish-growth'
+  }
+};
 
 @Component({
   selector: 'app-edge-chase-page',
@@ -17,5 +31,7 @@ import { CodeDisplayComponent } from 'src/app/layout/code-display/code-display.c
   styleUrls: ['./edge-chase-page.component.css']
 })
 export class EdgeChasePageComponent {
+
+  Pagination: PaginationData = PAGINATIONDATA;
 
 }
