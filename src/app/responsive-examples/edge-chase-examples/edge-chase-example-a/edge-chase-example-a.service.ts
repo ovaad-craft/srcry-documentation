c48ef42c558e163bddc24594d8a25493d3455a20
Optiq01 <Optiqvision@gmail.com>
2023-09-24 01:04:16 -0500

    create function for creating broadcastChannel

--- src/app/responsive-examples/edge-chase-examples/edge-chase-example-a/edge-chase-example-a.service.ts
+++ src/app/responsive-examples/edge-chase-examples/edge-chase-example-a/edge-chase-example-a.service.ts
@@ -10,4 +10,10 @@ export class EdgeChaseExampleAService {
   TargetName!: string;
 
   constructor(private zone: NgZone) { }
+
+  public createBroadcastChannel(dataChannelName: string, channelName: string, targetName: string): void{
+    this.DataChannel = new BroadcastChannel(dataChannelName);
+    this.ChannelName = channelName;
+    this.TargetName = targetName;
+  }
 }
