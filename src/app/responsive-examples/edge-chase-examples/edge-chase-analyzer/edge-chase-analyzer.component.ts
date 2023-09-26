04b05bbababa927496ed18d89e761b8c98e3390e
Optiq01 <Optiqvision@gmail.com>
2023-09-26 18:55:43 -0500

    create inputs for broadcastChannel and import service and lifecycle hooks

--- src/app/responsive-examples/edge-chase-examples/edge-chase-analyzer/edge-chase-analyzer.component.ts
+++ src/app/responsive-examples/edge-chase-examples/edge-chase-analyzer/edge-chase-analyzer.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EdgeChaseAnalyzerService } from './edge-chase-analyzer.service';
 
 @Component({
   selector: 'app-edge-chase-analyzer',
@@ -8,6 +9,24 @@ import { CommonModule } from '@angular/common';
   templateUrl: './edge-chase-analyzer.component.html',
   styleUrls: ['./edge-chase-analyzer.component.css']
 })
-export class EdgeChaseAnalyzerComponent {
+export class EdgeChaseAnalyzerComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  @Input() BroadcastName!: string;
+  @Input() ChannelName!: string;
+  @Input() TargetName!: string;
+
+  constructor(private dataService: EdgeChaseAnalyzerService){}
+
+  ngOnInit(): void {
+      
+  }
+
+  ngAfterViewInit(): void {
+      
+  }
+
+  ngOnDestroy(): void {
+      
+  }
 
 }
