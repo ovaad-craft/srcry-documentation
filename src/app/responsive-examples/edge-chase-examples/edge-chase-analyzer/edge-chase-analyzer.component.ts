d1c10eb3df39dba83c53ad19b8e6af95e28e0a66
Optiq01 <Optiqvision@gmail.com>
2023-09-27 00:59:37 -0500

    make encapsulation shadow dom

--- src/app/responsive-examples/edge-chase-examples/edge-chase-analyzer/edge-chase-analyzer.component.ts
+++ src/app/responsive-examples/edge-chase-examples/edge-chase-analyzer/edge-chase-analyzer.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EdgeChaseAnalyzerService } from './edge-chase-analyzer.service';
 import { EdgeChaseSettings } from '@site-types';
@@ -8,7 +8,8 @@ import { EdgeChaseSettings } from '@site-types';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './edge-chase-analyzer.component.html',
-  styleUrls: ['./edge-chase-analyzer.component.css']
+  styleUrls: ['./edge-chase-analyzer.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class EdgeChaseAnalyzerComponent implements OnInit, AfterViewInit, OnDestroy {
 
