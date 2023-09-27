67264725ab7da5e9c097c9c5208434edc1a5e098
Optiq01 <Optiqvision@gmail.com>
2023-09-26 19:35:03 -0500

    create variables for broadcast channel and data

--- src/app/responsive-examples/edge-chase-examples/edge-chase-analyzer/edge-chase-analyzer.service.ts
+++ src/app/responsive-examples/edge-chase-examples/edge-chase-analyzer/edge-chase-analyzer.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { SrcryPropReadings } from '@site-types';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EdgeChaseAnalyzerService {
+
+  private DataChannel!: BroadcastChannel;
+  private ChannelName!: string;
+  private TargetName!: string;
+
+  private Readings: BehaviorSubject<SrcryPropReadings> = new BehaviorSubject<SrcryPropReadings>({
+    activePropW: '--',
+    activePropH: '--',
+    width: 0,
+    height: 0
+  });
+
+  public Readings$ = this.Readings.asObservable();
 
   constructor() { }
 }
