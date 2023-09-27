0e6bf293f266bd8a0fcef34577a30d5c04a779b9
Optiq01 <Optiqvision@gmail.com>
2023-09-26 23:32:51 -0500

    create variables for broadcast channel and data

--- src/app/responsive-examples/edge-chase-examples/edge-chase-analyzer/edge-chase-controller/edge-chase-controller.service.ts
+++ src/app/responsive-examples/edge-chase-examples/edge-chase-analyzer/edge-chase-controller/edge-chase-controller.service.ts
@@ -1,9 +1,55 @@
 import { Injectable } from '@angular/core';
+import { EdgeChaseData, SrcryPropReadings } from '@site-types';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EdgeChaseControllerService {
+
+  private DataChannel!: BroadcastChannel;
+  private ChannelName!: string;
+  private TargetName!: string;
+
+  private Readings: BehaviorSubject<SrcryPropReadings> = new BehaviorSubject<SrcryPropReadings>({
+    activePropW: '--',
+    activePropH: '--',
+    width: 0,
+    height: 0
+  });
+
+  public Readings$ = this.Readings.asObservable();
+
+  public DefaultSettings: EdgeChaseData = {
+    edgeChaseW: {
+      size: 'tiny',
+      scale: '2',
+      speed: '8'
+    },
+    edgeChaseWNudgeChunk: 0,
+    edgeChaseWNudgeSlice: 0,
+    edgeChaseH: {
+      size: 'tiny',
+      scale: '2',
+      speed: '8'
+    },
+    edgeChaseHNudgeChunk: 0,
+    edgeChaseHNudgeSlice: 0,
+    chaseStopW: {
+      size: 'hiMed',
+      scale: '1',
+      speed: '6'
+    },
+    chaseStopWNudgeChunk: 0,
+    chaseStopWNudgeSlice: 0,
+    chaseStopH: {
+      size: 'loMed',
+      scale: '2',
+      speed: '4'
+    },
+    chaseStopHNudgeChunk: 0,
+    chaseStopHNudgeSlice: 0
+  }
 
   constructor() { }
 }
