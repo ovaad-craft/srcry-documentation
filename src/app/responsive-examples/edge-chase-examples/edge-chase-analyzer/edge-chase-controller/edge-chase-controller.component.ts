c58193e63f4a55c840cdd1e4908eb5eaecfb38fa
Optiq01 <Optiqvision@gmail.com>
2023-09-27 00:25:53 -0500

    subscribe to readings, get default settings and create broadcastChannel in OnInit hook

--- src/app/responsive-examples/edge-chase-examples/edge-chase-analyzer/edge-chase-controller/edge-chase-controller.component.ts
+++ src/app/responsive-examples/edge-chase-examples/edge-chase-analyzer/edge-chase-controller/edge-chase-controller.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EdgeChaseControllerService } from './edge-chase-controller.service';
+import { EdgeChaseData, EdgeChaseSettings, SrcryPropReadings } from '@site-types';
 
 @Component({
   selector: 'edge-chase-controller',
@@ -15,10 +16,15 @@ export class EdgeChaseControllerComponent implements OnInit, AfterViewInit, OnDe
   @Input() ChannelName!: string;
   @Input() TargetName!: string;
 
+  DefaultSettings!: EdgeChaseData;
+  Readings!: SrcryPropReadings;
+
   constructor(private dataService: EdgeChaseControllerService){}
 
   ngOnInit(): void {
-      
+    this.dataService.Readings$.subscribe(a=> this.Readings = a);
+    this.DefaultSettings = this.dataService.getDefaultSettings();
+    this.dataService.createBroadcastChannel(this.BrodcastName, this.ChannelName, this.TargetName);
   }
 
   ngAfterViewInit(): void {
