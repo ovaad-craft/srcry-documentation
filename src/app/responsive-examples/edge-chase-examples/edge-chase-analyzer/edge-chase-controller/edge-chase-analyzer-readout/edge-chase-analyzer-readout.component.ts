9b22be1ef1a011fabe51e9aab0b3aa1ee278de36
Optiq01 <Optiqvision@gmail.com>
2023-10-02 15:50:52 -0500

    markup and style readout component

--- src/app/responsive-examples/edge-chase-examples/edge-chase-analyzer/edge-chase-controller/edge-chase-analyzer-readout/edge-chase-analyzer-readout.component.ts
+++ src/app/responsive-examples/edge-chase-examples/edge-chase-analyzer/edge-chase-controller/edge-chase-analyzer-readout/edge-chase-analyzer-readout.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SrcryPropReadings } from '@site-types';
 
 @Component({
-  selector: 'app-edge-chase-analyzer-readout',
+  selector: 'edge-chase-analyzer-readout',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './edge-chase-analyzer-readout.component.html',
   styleUrls: ['./edge-chase-analyzer-readout.component.css']
 })
 export class EdgeChaseAnalyzerReadoutComponent {
+
+  @Input()Readings!: SrcryPropReadings;
 
 }
