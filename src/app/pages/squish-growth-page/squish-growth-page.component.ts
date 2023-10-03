93df23cec4f60eeba48875c0b6cbd83f699eb76f
Optiq01 <Optiqvision@gmail.com>
2023-10-03 17:45:21 -0500

    import pagination, chart, demonstration and code display components

--- src/app/pages/squish-growth-page/squish-growth-page.component.ts
+++ src/app/pages/squish-growth-page/squish-growth-page.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PaginateComponent } from 'src/app/layout/paginate/paginate.component';
+import { PropChartComponent } from 'src/app/layout/prop-chart/prop-chart.component';
+import { DemonstrationFrameComponent } from 'src/app/layout/demonstration-frame/demonstration-frame.component';
+import { CodeDisplayComponent } from 'src/app/layout/code-display/code-display.component';
 
 @Component({
   selector: 'app-squish-growth-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    PaginateComponent,
+    PropChartComponent,
+    DemonstrationFrameComponent,
+    CodeDisplayComponent
+  ],
   templateUrl: './squish-growth-page.component.html',
   styleUrls: ['./squish-growth-page.component.css']
 })
