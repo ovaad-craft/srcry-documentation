ef9088a9219afd4590cd72577b20451930fd3071
Optiq01 <Optiqvision@gmail.com>
2023-10-06 16:58:41 -0500

    create inputs for broadcast names, import service and make encapsulation use shadow dom

--- src/app/responsive-examples/squish-growth-examples/squish-growth-example-a/squish-growth-example-a.component.ts
+++ src/app/responsive-examples/squish-growth-examples/squish-growth-example-a/squish-growth-example-a.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SquishGrowthExampleAService } from './squish-growth-example-a.service';
 
 @Component({
   selector: 'app-squish-growth-example-a',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './squish-growth-example-a.component.html',
-  styleUrls: ['./squish-growth-example-a.component.css']
+  styleUrls: ['./squish-growth-example-a.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class SquishGrowthExampleAComponent {
+  @Input()BroadcastName!: string;
+  @Input()ChannelName!: string;
+  @Input()TargetName!: string;
+
+  constructor(private dataService: SquishGrowthExampleAService){}
 
 }
