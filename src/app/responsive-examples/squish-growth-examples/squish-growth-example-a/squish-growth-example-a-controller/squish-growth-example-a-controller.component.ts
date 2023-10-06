2a3401b90ea012af061563e085d461fbd65d29fe
Optiq01 <Optiqvision@gmail.com>
2023-10-06 14:21:45 -0500

    create squish growth example a

---
+++ src/app/responsive-examples/squish-growth-examples/squish-growth-example-a/squish-growth-example-a-controller/squish-growth-example-a-controller.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-squish-growth-example-a-controller',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './squish-growth-example-a-controller.component.html',
+  styleUrls: ['./squish-growth-example-a-controller.component.css']
+})
+export class SquishGrowthExampleAControllerComponent {
+
+}
---
+++ src/app/responsive-examples/squish-growth-examples/squish-growth-example-a/squish-growth-example-a.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-squish-growth-example-a',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './squish-growth-example-a.component.html',
+  styleUrls: ['./squish-growth-example-a.component.css']
+})
+export class SquishGrowthExampleAComponent {
+
+}
