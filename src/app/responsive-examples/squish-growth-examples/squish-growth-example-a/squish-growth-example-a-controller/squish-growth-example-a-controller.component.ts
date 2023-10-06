ff653db436359dc314a4182ee8b22b5e21dbc38f
Optiq01 <Optiqvision@gmail.com>
2023-10-06 16:49:01 -0500

    create inputs for broadcast channel names and import service

--- src/app/responsive-examples/squish-growth-examples/squish-growth-example-a/squish-growth-example-a-controller/squish-growth-example-a-controller.component.ts
+++ src/app/responsive-examples/squish-growth-examples/squish-growth-example-a/squish-growth-example-a-controller/squish-growth-example-a-controller.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SquishGrowthExampleAService } from '../squish-growth-example-a.service';
 
 @Component({
-  selector: 'app-squish-growth-example-a-controller',
+  selector: 'squish-growth-example-a-controller',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './squish-growth-example-a-controller.component.html',
   styleUrls: ['./squish-growth-example-a-controller.component.css']
 })
 export class SquishGrowthExampleAControllerComponent {
+
+  @Input()BroadcastName!: string;
+  @Input()ChannelName!: string;
+  @Input()TargetName!: string;
+
+  constructor(private dataService: SquishGrowthExampleAService){}
 
 }
