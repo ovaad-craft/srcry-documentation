25c49885e37fab0b1e685a5ca89d9d5bb2eb9105
Optiq01 <Optiqvision@gmail.com>
2023-10-08 00:45:28 -0500

    create functions for creating broadcast channel, sending data and closing channel

--- src/app/responsive-examples/squish-growth-examples/squish-growth-example-a/squish-growth-example-a.service.ts
+++ src/app/responsive-examples/squish-growth-examples/squish-growth-example-a/squish-growth-example-a.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+import { SquishGrowthReadings } from '@site-types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SquishGrowthExampleAService {
 
-  constructor() { }
+  DataChannel!: BroadcastChannel;
+  ChannelName!: string;
+  TargetName!: string;
+
+  constructor(private zone: NgZone) { }
+
+  public createChannel(dataChannelName: string, channelName: string, targetName: string): void{
+    this. DataChannel = new BroadcastChannel(dataChannelName);
+    this.ChannelName = channelName;
+    this.TargetName = targetName;
+  }
+
+  public sendData(data: SquishGrowthReadings): void{
+    this.zone.run(()=>{
+      this.DataChannel.postMessage({
+        target: this.TargetName,
+        data: data
+      });
+    });
+  }
+
+  public closeChannel():void{
+    this.DataChannel.close();
+  }
+
+
 }
