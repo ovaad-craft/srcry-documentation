a00803706da3c93a4447cb6d040621004e0e7b1d
Optiq01 <Optiqvision@gmail.com>
2023-10-09 16:39:20 -0500

    import readout and input components

--- src/app/responsive-examples/squish-growth-examples/squish-growth-analyzer/squish-growth-analyzer-controller/squish-growth-analyzer-controller.component.ts
+++ src/app/responsive-examples/squish-growth-examples/squish-growth-analyzer/squish-growth-analyzer-controller/squish-growth-analyzer-controller.component.ts
@@ -1,11 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SquishGrowthAnalyzerControllerService } from './squish-growth-analyzer-controller.service';
+import { SquishGrowthAnalyzerReadoutComponent } from './squish-growth-analyzer-readout/squish-growth-analyzer-readout.component';
+import { SquishGrowthAnalyzerInputComponent } from './squish-growth-analyzer-input/squish-growth-analyzer-input.component';
+import { SquishGrowthAnalyzerReadings } from '@site-types';
 
 @Component({
   selector: 'squish-growth-analyzer-controller',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    SquishGrowthAnalyzerReadoutComponent,
+    SquishGrowthAnalyzerInputComponent
+  ],
   templateUrl: './squish-growth-analyzer-controller.component.html',
   styleUrls: ['./squish-growth-analyzer-controller.component.css']
 })
@@ -15,10 +22,12 @@ export class SquishGrowthAnalyzerControllerComponent implements OnInit {
   @Input()ChannelName!: string;
   @Input()TargetName!: string;
 
+  Readings!: SquishGrowthAnalyzerReadings;
+
   constructor(private dataService: SquishGrowthAnalyzerControllerService){}
 
   ngOnInit(): void {
-      
+    this.dataService.Readings$.subscribe(a=> this.Readings = a);
   }
 
 }
