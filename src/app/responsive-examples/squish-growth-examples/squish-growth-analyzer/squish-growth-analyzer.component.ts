dc064174d848d27b4ae0867599c4977e5596691f
Optiq01 <Optiqvision@gmail.com>
2023-10-08 23:14:41 -0500

    create Inputs and create broadcast channel

--- src/app/responsive-examples/squish-growth-examples/squish-growth-analyzer/squish-growth-analyzer.component.ts
+++ src/app/responsive-examples/squish-growth-examples/squish-growth-analyzer/squish-growth-analyzer.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy, Input, ViewChild, ElementRef, NgZone } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SquishGrowthSettings } from '@site-types';
+import { SquishGrowthAnalyzerService } from './squish-growth-analyzer.service';
 
 @Component({
   selector: 'app-squish-growth-analyzer',
@@ -8,6 +10,26 @@ import { CommonModule } from '@angular/common';
   templateUrl: './squish-growth-analyzer.component.html',
   styleUrls: ['./squish-growth-analyzer.component.css']
 })
-export class SquishGrowthAnalyzerComponent {
+export class SquishGrowthAnalyzerComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  @Input()BroadcastName!: string;
+  @Input()ChannelName!: string;
+  @Input()TargetName!: string;
+
+  Settings!: SquishGrowthSettings;
+
+  constructor(private zone: NgZone, private dataService: SquishGrowthAnalyzerService){}
+
+  ngOnInit(): void {
+    this.dataService.Settings$.subscribe(a=> this.Settings = a);
+    this.dataService.createDataChannel(this.BroadcastName, this.ChannelName, this.TargetName);
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+  ngOnDestroy(): void {
+      this.dataService.closeChannel();
+  }
 
 }
