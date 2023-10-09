14cd779a7868eccef2c57dbc1dcabe72538e0f1a
Optiq01 <Optiqvision@gmail.com>
2023-10-09 16:43:11 -0500

    create input for Readings

--- src/app/responsive-examples/squish-growth-examples/squish-growth-analyzer/squish-growth-analyzer-controller/squish-growth-analyzer-readout/squish-growth-analyzer-readout.component.ts
+++ src/app/responsive-examples/squish-growth-examples/squish-growth-analyzer/squish-growth-analyzer-controller/squish-growth-analyzer-readout/squish-growth-analyzer-readout.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SquishGrowthAnalyzerReadings } from '@site-types';
 
 @Component({
-  selector: 'app-squish-growth-analyzer-readout',
+  selector: 'squish-growth-analyzer-readout',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './squish-growth-analyzer-readout.component.html',
   styleUrls: ['./squish-growth-analyzer-readout.component.css']
 })
 export class SquishGrowthAnalyzerReadoutComponent {
+
+  @Input()Readings!: SquishGrowthAnalyzerReadings;
 
 }
