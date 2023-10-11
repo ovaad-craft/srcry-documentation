3da1a32f922d1e8bbb57e56c377bd2373804673e
Optiq01 <Optiqvision@gmail.com>
2023-10-10 20:20:21 -0500

    import prop button, number input and box size selector components

--- src/app/responsive-examples/squish-growth-examples/squish-growth-analyzer/squish-growth-analyzer-controller/squish-growth-analyzer-input/squish-growth-analyzer-input.component.ts
+++ src/app/responsive-examples/squish-growth-examples/squish-growth-analyzer/squish-growth-analyzer-controller/squish-growth-analyzer-input/squish-growth-analyzer-input.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SrcryPropButtonComponent } from 'src/app/layout/srcry-prop-button/srcry-prop-button.component';
+import { NumberInputComponent } from 'src/app/layout/number-input/number-input.component';
+import { BoxSizeSelectorComponent } from 'src/app/responsive-examples/box-size-examples/box-size-analyzer/analyzer-input/box-size-selector/box-size-selector.component';
 
 @Component({
-  selector: 'app-squish-growth-analyzer-input',
+  selector: 'squish-growth-analyzer-input',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    SrcryPropButtonComponent,
+    NumberInputComponent,
+    BoxSizeSelectorComponent
+  ],
   templateUrl: './squish-growth-analyzer-input.component.html',
   styleUrls: ['./squish-growth-analyzer-input.component.css']
 })
