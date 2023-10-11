bea0cffef8c0939d8dc872cd4dbb40b3921ba5b8
Optiq01 <Optiqvision@gmail.com>
2023-10-10 23:46:41 -0500

    create functions for updating properties

--- src/app/responsive-examples/squish-growth-examples/squish-growth-analyzer/squish-growth-analyzer-controller/squish-growth-analyzer-input/squish-growth-analyzer-input.component.ts
+++ src/app/responsive-examples/squish-growth-examples/squish-growth-analyzer/squish-growth-analyzer-controller/squish-growth-analyzer-input/squish-growth-analyzer-input.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SrcryPropButtonComponent } from 'src/app/layout/srcry-prop-button/srcry-prop-button.component';
 import { NumberInputComponent } from 'src/app/layout/number-input/number-input.component';
 import { BoxSizeSelectorComponent } from 'src/app/responsive-examples/box-size-examples/box-size-analyzer/analyzer-input/box-size-selector/box-size-selector.component';
-import { SquishGrowthData, SquishGrowthProps } from '@site-types';
+import { BoxSizeProps, SquishGrowthData, SquishGrowthProps } from '@site-types';
 import { createBoxSize } from 'src/app/utils/create-box-size';
 
 @Component({
@@ -58,6 +58,10 @@ export class SquishGrowthAnalyzerInputComponent implements OnInit {
     };
   }
 
+  private updateSettings():void{
+    this.UpdateProps.emit(this.PropSettings);
+  }
+
   public dimensionToggler(value: boolean):void{
     this.DimensionToggle = value;
   }
@@ -65,41 +69,91 @@ export class SquishGrowthAnalyzerInputComponent implements OnInit {
   public toggleSquishGrowthWStart():void{
     this.ToggleSquishGrowthWStart = !this.ToggleSquishGrowthWStart;
   }
+
+  public updateSquishGrowthWStart(value: number):void{
+    this.PropSettings.squishGrowthWStart = `${value}px`;
+    this.updateSettings();
+  }
   
   public toggleSquishGrowthWSpeed():void{
     this.ToggleSquishGrowthWSpeed = !this.ToggleSquishGrowthWSpeed;
   }
   
+  public updateSquishGrowthWSpeed(value: number):void{
+    this.PropSettings.squishGrowthWSpeed = value;
+    this.updateSettings();
+  }
+  
   public toggleSquishGrowthWMax():void{
     this.ToggleSquishGrowthWMax = !this.ToggleSquishGrowthWMax;
+  }
+
+  public updateSquishGrowthWMax(value: BoxSizeProps):void{
+    this.PropSettings.squishGrowthWMax = value;
+    this.updateSettings();
   }
   
   public toggleSquishGrowthWMaxNudgeScale():void{
     this.ToggleSquishGrowthWMaxNudgeScale = !this.ToggleSquishGrowthWMaxNudgeScale;
+  }
+
+  public updateSquishGrowthWMaxNudgeScale(value: number): void{
+    this.PropSettings.squishGrowthWMaxNudgeScale = value;
+    this.updateSettings();
   }
   
   public toggleSquishGrowthWMaxNudgeSlice():void{
     this.ToggleSquishGrowthWMaxNudgeSlice = !this.ToggleSquishGrowthWMaxNudgeSlice;
   }
   
+  public updateSquishGrowthWMaxNudgeSlice(value: number): void{
+    this.PropSettings.squishGrowthWMaxNudgeSlice = value;
+    this.updateSettings();
+  }
+  
   public toggleSquishGrowthHStart():void{
     this.ToggleSquishGrowthHStart = !this.ToggleSquishGrowthHStart;
+  }
+
+  public updateSquishGrowthHStart(value: number):void{
+    this.PropSettings.squishGrowthHStart = `${value}px`;
+    this.updateSettings();
   }
   
   public toggleSquishGrowthHSpeed():void{
     this.ToggleSquishGrowthHSpeed = !this.ToggleSquishGrowthHSpeed;
   }
+
+  public updateSquishGrowthHSpeed(value: number):void{
+    this.PropSettings.squishGrowthHSpeed = value;
+    this.updateSettings();
+  }
   
   public toggleSquishGrowthHMax():void{
     this.ToggleSquishGrowthHMax = !this.ToggleSquishGrowthHMax;
+  }
+
+  public updateSquishGrowthHMax(value: BoxSizeProps):void{
+    this.PropSettings.squishGrowthHMax = value;
+    this.updateSettings();
   }
   
   public toggleSquishGrowthHMaxNudgeScale():void{
     this.ToggleSquishGrowthHMaxNudgeScale = !this.ToggleSquishGrowthHMaxNudgeScale;
   }
+
+  public updateSquishGrowthHMaxNudgeScale(value: number): void{
+    this.PropSettings.squishGrowthHMaxNudgeScale = value;
+    this.updateSettings();
+  }
   
   public toggleSquishGrowthHMaxNudgeSlice():void{
     this.ToggleSquishGrowthHMaxNudgeSlice = !this.ToggleSquishGrowthHMaxNudgeSlice;
+  }
+
+  public updateSquishGrowthHMaxNudgeSlice(value: number): void{
+    this.PropSettings.squishGrowthHMaxNudgeSlice = value;
+    this.updateSettings();
   }
 
 }
