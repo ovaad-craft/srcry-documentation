7e663ea71ad4ddb36f14e76959f16f6bab902491
Optiq01 <Optiqvision@gmail.com>
2023-10-10 20:31:10 -0500

    create variable for prop settings and function for shaping default data into values

--- src/app/responsive-examples/squish-growth-examples/squish-growth-analyzer/squish-growth-analyzer-controller/squish-growth-analyzer-input/squish-growth-analyzer-input.component.ts
+++ src/app/responsive-examples/squish-growth-examples/squish-growth-analyzer/squish-growth-analyzer-controller/squish-growth-analyzer-input/squish-growth-analyzer-input.component.ts
@@ -1,9 +1,10 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SrcryPropButtonComponent } from 'src/app/layout/srcry-prop-button/srcry-prop-button.component';
 import { NumberInputComponent } from 'src/app/layout/number-input/number-input.component';
 import { BoxSizeSelectorComponent } from 'src/app/responsive-examples/box-size-examples/box-size-analyzer/analyzer-input/box-size-selector/box-size-selector.component';
 import { SquishGrowthData, SquishGrowthProps } from '@site-types';
+import { createBoxSize } from 'src/app/utils/create-box-size';
 
 @Component({
   selector: 'squish-growth-analyzer-input',
@@ -17,9 +18,31 @@ import { SquishGrowthData, SquishGrowthProps } from '@site-types';
   templateUrl: './squish-growth-analyzer-input.component.html',
   styleUrls: ['./squish-growth-analyzer-input.component.css']
 })
-export class SquishGrowthAnalyzerInputComponent {
+export class SquishGrowthAnalyzerInputComponent implements OnInit {
 
   @Input() DefaultSettings!: SquishGrowthData;
   @Output() UpdateProps: EventEmitter<SquishGrowthProps> = new EventEmitter<SquishGrowthProps>();
+
+  PropSettings!: SquishGrowthProps;
+
+  ngOnInit(): void {
+      this.initSettings(this.DefaultSettings);
+  }
+
+  private initSettings(data: SquishGrowthData): SquishGrowthProps{
+    return {
+      ...data,
+      squishGrowthWMax: createBoxSize(
+        data.squishGrowthWMax.size,
+        data.squishGrowthWMax.scale,
+        data.squishGrowthWMax.speed,
+      ),
+      squishGrowthHMax: createBoxSize(
+        data.squishGrowthHMax.size,
+        data.squishGrowthHMax.scale,
+        data.squishGrowthHMax.speed,
+      )
+    };
+  }
 
 }
