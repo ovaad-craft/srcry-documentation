dcf18c2c180bb105d3de61190d78541026df95e8
Optiq01 <Optiqvision@gmail.com>
2023-10-16 22:26:49 -0500

    add functionality for creating broadcast channel, sending data and closing channel

--- src/app/responsive-examples/stretch-shrink-examples/stretch-shrink-example-a/stretch-shrink-example-a.service.ts
+++ src/app/responsive-examples/stretch-shrink-examples/stretch-shrink-example-a/stretch-shrink-example-a.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+import { StretchShrinkReadings } from '@site-types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StretchShrinkExampleAService {
+  DataChannel!: BroadcastChannel;
+  ChannelName!: string;
+  TargetName!: string;
 
-  constructor() { }
+  constructor(private zone: NgZone) { }
+
+  public createChannel(broadcastName: string, channelName: string, targetName: string): void{
+    this.DataChannel = new BroadcastChannel(broadcastName);
+    this.ChannelName = channelName;
+    this.TargetName = targetName;
+  }
+
+  public sendData(data: StretchShrinkReadings):void{
+    this.zone.run(()=>{
+      this.DataChannel.postMessage({
+        target: this.TargetName,
+        payload: data
+      });
+    });
+  }
+
+  public closeChannel():void{
+    this.DataChannel.close();
+  }
 }
