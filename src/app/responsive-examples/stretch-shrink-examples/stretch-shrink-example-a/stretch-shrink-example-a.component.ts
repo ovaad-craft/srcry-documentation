b591148fbf987bd9ced973dc4b0b6b30364540ac
Optiq01 <Optiqvision@gmail.com>
2023-10-16 22:41:55 -0500

    create broadcast channel in OnInit hook and logic for listening to changes in element size

--- src/app/responsive-examples/stretch-shrink-examples/stretch-shrink-example-a/stretch-shrink-example-a.component.ts
+++ src/app/responsive-examples/stretch-shrink-examples/stretch-shrink-example-a/stretch-shrink-example-a.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild, ViewEncapsulation, OnInit, AfterViewInit, OnDestroy, NgZone } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StretchShrinkExampleAService } from './stretch-shrink-example-a.service';
 
@@ -10,7 +10,7 @@ import { StretchShrinkExampleAService } from './stretch-shrink-example-a.service
   styleUrls: ['./stretch-shrink-example-a.component.css'],
   encapsulation: ViewEncapsulation.ShadowDom
 })
-export class StretchShrinkExampleAComponent {
+export class StretchShrinkExampleAComponent implements OnInit, AfterViewInit, OnDestroy {
 
   @Input() BroadcastName!: string;
   @Input() ChannelName!: string;
@@ -20,6 +20,29 @@ export class StretchShrinkExampleAComponent {
   @ViewChild('box', {static: true, read: ElementRef}) Box!: ElementRef;
   @ViewChild('shrinkBox', {static: true, read: ElementRef}) ShrinkBox!: ElementRef;
 
-  constructor(private dataService: StretchShrinkExampleAService){}
+  constructor(private dataService: StretchShrinkExampleAService, private zone: NgZone){}
+
+  ngOnInit(): void {
+    this.dataService.createChannel(this.BroadcastName, this.ChannelName, this.TargetName);
+  }
+
+  ngAfterViewInit(): void {
+    const frameListener: ResizeObserver = new ResizeObserver((element)=>{
+      this.zone.run(()=> this.readElements());
+    });
+
+    frameListener.observe(this.BoxFrame.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    this.dataService.closeChannel();
+  }
+
+  private readElements():void{
+    this.dataService.sendData({
+      height: this.Box.nativeElement.offsetHeight,
+      ShrinkAmountH: this.ShrinkBox.nativeElement.offsetHeight
+    });
+  }
 
 }
