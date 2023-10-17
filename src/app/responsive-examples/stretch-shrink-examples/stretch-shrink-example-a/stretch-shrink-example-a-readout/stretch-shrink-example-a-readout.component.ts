88df227389a044fe2e3779ab531ce305aaf3e5c2
Optiq01 <Optiqvision@gmail.com>
2023-10-16 23:29:36 -0500

    create functionality for setting up and closing broadcast channel and update readings

--- src/app/responsive-examples/stretch-shrink-examples/stretch-shrink-example-a/stretch-shrink-example-a-readout/stretch-shrink-example-a-readout.component.ts
+++ src/app/responsive-examples/stretch-shrink-examples/stretch-shrink-example-a/stretch-shrink-example-a-readout/stretch-shrink-example-a-readout.component.ts
@@ -1,20 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StretchShrinkExampleAReadoutService } from './stretch-shrink-example-a-readout.service';
+import { StretchShrinkReadings } from '@site-types';
 
 @Component({
-  selector: 'app-stretch-shrink-example-a-readout',
+  selector: 'stretch-shrink-example-a-readout',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './stretch-shrink-example-a-readout.component.html',
   styleUrls: ['./stretch-shrink-example-a-readout.component.css']
 })
-export class StretchShrinkExampleAReadoutComponent {
+export class StretchShrinkExampleAReadoutComponent implements OnInit, OnDestroy {
 
   @Input() BroadcastName!: string;
   @Input() ChannelName!: string;
   @Input() TargetName!: string;
 
+  Readings!: StretchShrinkReadings;
+
   constructor(private dataService: StretchShrinkExampleAReadoutService){}
+
+  ngOnInit(): void {
+    this.dataService.Readings$.subscribe(a=> this.Readings = a);
+  }
+
+  ngOnDestroy(): void {
+      this.dataService.closeChannel();
+  }
 
 }
