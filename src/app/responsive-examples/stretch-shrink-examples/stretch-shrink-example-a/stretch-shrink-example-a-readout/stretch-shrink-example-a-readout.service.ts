9159d9edcb35ec3719e7a583e5b1cc0cf95e0bf8
Optiq01 <Optiqvision@gmail.com>
2023-10-16 23:03:44 -0500

    create functionality for recieving and updating readings from broadcast channel

--- src/app/responsive-examples/stretch-shrink-examples/stretch-shrink-example-a/stretch-shrink-example-a-readout/stretch-shrink-example-a-readout.service.ts
+++ src/app/responsive-examples/stretch-shrink-examples/stretch-shrink-example-a/stretch-shrink-example-a-readout/stretch-shrink-example-a-readout.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { StretchShrinkReadings } from '@site-types';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StretchShrinkExampleAReadoutService {
 
+  DataChannel!: BroadcastChannel;
+  ChannelName!: string;
+  TargetName!: string;
+
+  private Readings: BehaviorSubject<StretchShrinkReadings> = new BehaviorSubject<StretchShrinkReadings>({
+    height: 0,
+    ShrinkAmountH: 0
+  });
+
+  public Readings$ = this.Readings.asObservable();
+
   constructor() { }
+
+  private setChannelListener():void{
+    this.DataChannel.onmessage = (event)=>{
+      if(event.data.target === this.ChannelName){
+        this.Readings.next(event.data.payload);
+      }
+    };
+  }
+
+  public createChannel(broadcastName: string, channelName: string, targetName: string):void{
+    this.DataChannel = new BroadcastChannel(broadcastName);
+    this.ChannelName = channelName;
+    this.TargetName = targetName;
+
+    this.setChannelListener();
+  }
 }
