ded60aaa88dcd87b1b6739e18394050e118a3351
Optiq01 <Optiqvision@gmail.com>
2023-10-16 22:28:10 -0500

    create ViewChild references to elements and set encapsulation to shadow dom

--- src/app/responsive-examples/stretch-shrink-examples/stretch-shrink-example-a/stretch-shrink-example-a.component.ts
+++ src/app/responsive-examples/stretch-shrink-examples/stretch-shrink-example-a/stretch-shrink-example-a.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StretchShrinkExampleAService } from './stretch-shrink-example-a.service';
 
@@ -7,13 +7,18 @@ import { StretchShrinkExampleAService } from './stretch-shrink-example-a.service
   standalone: true,
   imports: [CommonModule],
   templateUrl: './stretch-shrink-example-a.component.html',
-  styleUrls: ['./stretch-shrink-example-a.component.css']
+  styleUrls: ['./stretch-shrink-example-a.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class StretchShrinkExampleAComponent {
 
   @Input() BroadcastName!: string;
   @Input() ChannelName!: string;
   @Input() TargetName!: string;
+
+  @ViewChild('boxFrame', {static: true, read: ElementRef}) BoxFrame!: ElementRef;
+  @ViewChild('box', {static: true, read: ElementRef}) Box!: ElementRef;
+  @ViewChild('shrinkBox', {static: true, read: ElementRef}) ShrinkBox!: ElementRef;
 
   constructor(private dataService: StretchShrinkExampleAService){}
 
