e3ad8cc2a2d542caa47d34d1f6bf2a4521ae2cdb
Optiq01 <Optiqvision@gmail.com>
2023-10-20 23:54:53 -0500

    create srcryText overview page

--- src/app/pages/srcry-text-overview-page/srcry-text-overview-page.component.ts
+++ src/app/pages/srcry-text-overview-page/srcry-text-overview-page.component.ts
@@ -1,13 +1,66 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PaginateComponent } from 'src/app/layout/paginate/paginate.component';
+import { RouterLink } from '@angular/router';
+import { CodeDisplayComponent } from 'src/app/layout/code-display/code-display.component';
+import { Link, PaginationData } from '@site-types';
+
+const PAGINATIONDATA: PaginationData = {
+  previous: {
+    id: 'stretchShrink',
+    title: 'Stretch Shrink',
+    path: '/stretch-shrink'
+  },
+  next: {
+    id: 'textSize',
+    title: 'Text Size',
+    path: '/text-size'
+  }
+}
+
+const LINKS: Link[] = [
+  {
+    id: 'textSize',
+    title: 'Text Size',
+    path: '/text-size'
+  },
+  {
+    id: 'kerningNudgeAmt',
+    title: 'Kerning Nudge Amount',
+    path: 'kerning-nudge-amt'
+  },
+  {
+    id: 'leadingNudgeAmt',
+    title: 'Leading Nudge Amount',
+    path: 'leading-nudge-amt'
+  }
+]
 
 @Component({
   selector: 'app-srcry-text-overview-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    PaginateComponent,
+    RouterLink,
+    CodeDisplayComponent
+  ],
   templateUrl: './srcry-text-overview-page.component.html',
   styleUrls: ['./srcry-text-overview-page.component.css']
 })
 export class SrcryTextOverviewPageComponent {
+
+  Pagination: PaginationData = PAGINATIONDATA;
+  Links: Link[] = LINKS;
+
+  Demo_01: string = ` <div class="srcryTxt myClass">
+    <!-- code -->
+ </div>
+  `;
+
+  Demo_02: string = ` .myClass{
+    /* define srcryTxt properies here */
+ }
+  `;
 
 }
