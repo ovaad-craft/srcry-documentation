ca4cd308ac21fb717d723f59899d80e4f81a9ad7
Optiq01 <Optiqvision@gmail.com>
2024-02-13 22:53:15 -0600

    add analytics trigger for github link

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GoogleAnalyticsService } from 'ngx-google-analytics';
 
 @Component({
   selector: 'navbar',
@@ -12,6 +13,12 @@ export class NavbarComponent {
 
   @Output() OpenSidebar : EventEmitter<boolean> = new EventEmitter<boolean>();
 
+  constructor(private gaService: GoogleAnalyticsService){}
+
   public openSidebar(): void{ this.OpenSidebar.emit(true); }
+
+  public launchRepo():void{
+    this.gaService.event('event', 'nav_repo_btn_clicked');
+  }
 
 }
