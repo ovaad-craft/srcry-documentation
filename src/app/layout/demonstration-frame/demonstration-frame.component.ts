5c24b657d43fb97de2c385edae0572d1d23e4a11
Optiq01 <Optiqvision@gmail.com>
2024-02-13 22:49:30 -0600

    remove afterRender hook from imports

--- src/app/layout/demonstration-frame/demonstration-frame.component.ts
+++ src/app/layout/demonstration-frame/demonstration-frame.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, afterRender } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResponsiveWindowComponent } from './responsive-window/responsive-window.component';
 import { FirefoxMessageComponent } from './firefox-message/firefox-message.component';
